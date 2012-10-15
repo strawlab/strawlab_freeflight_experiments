dfdd8d529413c3696a28cdcb69ddc454ae44c656
John Stowers <john.stowers@gmail.com>
2012-10-15 19:53:46 +0200

    nodelib: customizable path and store uuid

--- src/nodelib/log.py
+++ src/nodelib/log.py
@@ -11,16 +11,24 @@ class CsvLogger:
 
     STATE = tuple()
 
-    def __init__(self,fname=None, mode='w'):
+    def __init__(self,fname=None, mode='w', directory="~/FLYDRA"):
         assert len(self.STATE)
 
         self._flydra_data_file = ''
+        self._exp_uuid = ''
 
         for s in self.STATE:
             setattr(self, s, None)
 
         if fname is None:
-            self._fname = os.path.abspath(time.strftime('DATA%Y%m%d_%H%M%S.csv'))
+            directory = os.path.join(
+                            os.path.expanduser(directory),
+                            rospy.get_name()[1:])
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            self._fname = os.path.join(
+                            directory,
+                            time.strftime('DATA%Y%m%d_%H%M%S.csv'))
         else:
             self._fname = os.path.abspath(fname)
 
@@ -30,16 +38,22 @@ class CsvLogger:
             rospy.loginfo("writing %s" % self._fname)
             self._fd = open(self._fname,mode='w')
             self._fd.write(",".join(self.STATE))
-            self._fd.write(",t_sec,t_nsec,flydra_data_file\n")
+            self._fd.write(",t_sec,t_nsec,flydra_data_file,exp_uuid\n")
 
             rospy.Subscriber('flydra_mainbrain_data_file',
                              std_msgs.msg.String,
                              self._on_flydra_mainbrain_start_saving)
+            rospy.Subscriber('experiment/uuid',
+                             std_msgs.msg.String,
+                             self._on_experiment_uuid)
         else:
             raise Exception("mode must be 'r' or 'w'")
 
     def _on_flydra_mainbrain_start_saving(self, msg):
         self._flydra_data_file = msg.data
+
+    def _on_experiment_uuid(self, msg):
+        self._exp_uuid = msg.data
 
     def update(self, check=False):
         vals = [getattr(self,s) for s in self.STATE]
@@ -49,7 +63,7 @@ class CsvLogger:
 
         self._fd.write(",".join(map(str,vals)))
         t = rospy.get_rostime()
-        self._fd.write(",%d,%d,%s\n" % (t.secs,t.nsecs,self._flydra_data_file))
+        self._fd.write(",%d,%d,%s,%s\n" % (t.secs,t.nsecs,self._flydra_data_file,self._exp_uuid))
         self._fd.flush()
 
     def record_iterator(self):
