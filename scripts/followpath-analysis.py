2fabc16387c97e5db25b5776303e128d1cfe7959
John Stowers <john.stowers@gmail.com>
2012-12-10 13:58:32 +0100

    followpath: be more resiliant to types

--- scripts/followpath-analysis.py
+++ scripts/followpath-analysis.py
@@ -2,6 +2,7 @@ import os.path
 import sys
 import pickle
 import re
+import pprint
 
 sys.path.append('../nodes')
 import followpath
@@ -20,8 +21,8 @@ data = {}
 trial = {}
 figure_names = {}
 
-MIN_RATIO = .1
-PICKLE_THRESH = 0.5
+MIN_RATIO = 0.2
+PICKLE_THRESH = MIN_RATIO
 
 scalarMap = cmx.ScalarMappable(
                     norm=colors.Normalize(vmin=0, vmax=1),
@@ -50,21 +51,29 @@ for i in range(ncsv):
     rec = csv[i]
     try:
         lock_object = int(rec['lock_object'])
+
+        if lock_object > oid and lock_object != 0xFFFFFFFF:
+            oid = lock_object
+            data[oid] = []
+            trial[oid] = rec['condition']
+
+        if oid <= 0:
+            continue
+
+        if int(rec['active']):
+            t = nodelib.analysis.rec_get_time(rec)
+            data[oid].append( (float(rec['move_ratio']),
+                               float(rec['src_x']),
+                               float(rec['src_y']),
+                               float(rec['target_x']),
+                               float(rec['target_y']),
+                               t) )
+
     except ValueError:
-        print "invalid id", rec['lock_object']
+        print "invalid rec", len(rec)
+        pprint.pprint(rec)
         continue
 
-    if lock_object > oid and lock_object != 0xFFFFFFFF:
-        oid = lock_object
-        data[oid] = []
-        trial[oid] = rec['condition']
-
-    if oid <= 0:
-        continue
-
-    if int(rec['active']):
-        t = nodelib.analysis.rec_get_time(rec)
-        data[oid].append( (rec['move_ratio'],rec['src_x'],rec['src_y'],rec['target_x'],rec['target_y'],t) )
 
 svg_filename = str(rec['svg_filename'])
 
@@ -75,14 +84,14 @@ for oid in data:
     if len(xy) == 0:
         continue
     move_ratio = xy.max(axis=0)[0]
-    if move_ratio > MIN_RATIO or followpath.is_control_mode(trial[oid]):
-        if move_ratio > PICKLE_THRESH:
+    if move_ratio >= MIN_RATIO or followpath.is_control_mode(trial[oid]):
+        if move_ratio >= PICKLE_THRESH:
             topickle[oid] = (trial[oid],move_ratio,data[oid])
 
         figure_names[trial[oid]] = True
         plt.figure(trial[oid])
         plt.plot(xy[:,1],xy[:,2],
-                label='%d'%oid,
+                label='%d (%.1f)'%(oid,move_ratio),
                 color=scalarMap.to_rgba(move_ratio))
 
 svg = flyflypath.model.MovingPointSvgPath(svg_filename)
