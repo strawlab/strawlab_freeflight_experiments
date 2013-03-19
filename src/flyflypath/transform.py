887f42972cd5519e03f7b5e0e9d3901eaa81cc36
John Stowers <john.stowers@gmail.com>
2013-03-19 16:57:23 +0100

    flyflypath: fix SVG coordinate transform

--- src/flyflypath/transform.py
+++ src/flyflypath/transform.py
@@ -10,8 +10,8 @@ class SVGTransform:
         w = self._w
         c = self._w / 2
         #center of svg is at 250,250 - move 0,0 there
-        py = (y * +w * self._shrink) + c
-        px = (x * -w * self._shrink) + c
+        py = (y * -w * self._shrink) + c
+        px = (x * +w * self._shrink) + c
         return px,py
 
     def pxpy_to_xy(self, px,py):
@@ -20,3 +20,26 @@ class SVGTransform:
         y = (py - c) / -w
         x = (px - c) / +w
         return x/self._shrink, y/self._shrink
+
+    def m_to_pixel(self, m):
+        return m*self._w*self._shrink
+
+    def pixel_to_m(self, m):
+        return m/self._w/self._shrink
+
+
+if __name__ == "__main__":
+    XFORM = SVGTransform()
+
+    xy = [(0,0),(0.2,0.4),(-0.3,-0.1)]
+    for x,y in xy:
+        px,py = XFORM.xy_to_pxpy(x,y)
+        _x,_y = XFORM.pxpy_to_xy(px,py)
+        assert x == _x
+        assert y == _y
+
+    m = XFORM.pixel_to_m(20)
+    p = XFORM.m_to_pixel(m)
+
+    assert p == 20
+
