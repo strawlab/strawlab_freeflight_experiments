14bf84288217c931d2e74001997692b394ac435c
John Stowers <john.stowers@gmail.com>
2013-07-22 19:16:48 +0200

    movie: improve movie-osg-file
    * use new combine API
    * generalise to driving other Stimulus other than OSGFile
    * add ability to plot_xyz
    
    this actually looks quite nice now!

--- scripts/movies/movie-osgfile-virtualworld.py
+++ scripts/movies/movie-osgfile-virtualworld.py
@@ -2,9 +2,11 @@ import tables
 import numpy as np
 import scipy.misc
 import cv2
+import sys
 import os.path
 import tempfile
 import time
+import collections
 
 import benu.utils
 import benu.benu
@@ -22,12 +24,14 @@ import camera_model
 roslib.load_manifest('strawlab_freeflight_experiments')
 import analysislib.args
 import analysislib.movie
+import analysislib.combine
 import std_msgs.msg
 import geometry_msgs.msg
 
 roslib.load_manifest('flycave')
 import autodata.files
 import strawlab.constants
+from strawlab_freeflight_experiments.topics import *
 
 roslib.load_manifest('flyvr')
 import flyvr.display_client
@@ -37,11 +41,68 @@ VR_PANELS = ['virtual_world']
 TARGET_OUT_W, TARGET_OUT_H = 1024, 768
 MARGIN = 0
 
-def doit(h5_file, fmf_fname, obj_id, tmpdir, outdir, calibration, tfix, framenumber, sml, osg_file_desc):
-    h5 = tables.openFile(h5_file, mode='r')
-    trajectories = h5.root.trajectories
-    dt = 1.0/trajectories.attrs['frames_per_second']
-    trajectory_start_times = h5.root.trajectory_start_times
+class StimulusCylinderAndModel(flyvr.display_client.OSGFileStimulusSlave):
+    #the conflict format for this is
+    #justpost1.osg|-0.15|0.25|0.0
+    def __init__(self, dsc, osg_file_desc):
+        fname,x,y,z = osg_file_desc.split('|')
+        flyvr.display_client.OSGFileStimulusSlave.__init__(self, dsc, stimulus='StimulusCylinderAndModel')
+        self.set_model_filename(fname)
+        self.set_model_origin(map(float,(x,y,z)))
+
+        self.pub_rotation_velocity = rospy.Publisher(
+            self.dsc.name+'/' + TOPIC_CYL_ROTATION_RATE,
+            std_msgs.msg.Float32, latch=False, tcp_nodelay=True)
+
+    def set_state(self, row):
+        rrate = row['rotation_rate']
+        if not np.isnan(rrate):
+            self.pub_rotation_velocity.publish(rrate)
+
+class StimulusOSGFile(flyvr.display_client.OSGFileStimulusSlave):
+    #the format string for this looks like
+    #L.osgt/0.0,0.0,0.29/0.1,0.1,0.3
+    def __init__(self, dsc, osg_file_desc):
+        fname,oxyz,sxyz = osg_file_desc.split('/')
+        flyvr.display_client.OSGFileStimulusSlave.__init__(self, dsc)
+        self.set_model_filename(fname)
+        self.set_model_origin(map(float,oxyz.split(',')))
+        self.set_model_scale(map(float,sxyz.split(',')))
+
+    def set_state(self, row):
+        pass
+
+STIMULUS_CLASS_MAP = {
+    "StimulusOSGFile":StimulusOSGFile,
+    "StimulusCylinderAndModel":StimulusCylinderAndModel
+}
+
+STIMULUS_CSV_MAP = {
+    "StimulusOSGFile":None,
+    "StimulusCylinderAndModel":("conflict","conflict.csv",("ratio","rotation_rate")),
+}
+
+def doit(args, fmf_fname, obj_id, tmpdir, outdir, calibration, framenumber, sml, stimname, osg_file_desc, plot):
+    try:
+        logger,csvsuffix,csvparams = STIMULUS_CSV_MAP[stimname]
+
+        #YUCK - import the logger for the experiment
+        #FIXME: this could use the new logger constructor that takes
+        #a list of the csv file headings
+        sys.path.append(os.path.join(os.path.dirname(__file__),'..','..','nodes'))
+        klass = getattr(__import__(logger), "Logger")
+
+        combine = analysislib.combine.CombineH5WithCSV(
+                                klass,
+                                *csvparams
+        )
+        combine.add_from_args(args, csvsuffix)
+
+    except TypeError:
+        combine = analysislib.combine.CombineH5()
+        combine.add_from_args(args)
+
+    valid,dt,(x0,y0,obj_id,framenumber0,start) = combine.get_one_result(obj_id)
 
     renderers = {}
     osgslaves = {}
@@ -49,8 +110,11 @@ def doit(h5_file, fmf_fname, obj_id, tmpdir, outdir, calibration, tfix, framenum
         node = "/ds_%s" % name
         dsc = flyvr.display_client.DisplayServerProxy(display_server_node_name=node,wait=True)
 
+        stimklass = STIMULUS_CLASS_MAP[stimname]
+        print "rendering vr",name,stimklass
+
         renderers[name] = flyvr.display_client.RenderFrameSlave(dsc)
-        osgslaves[name] = flyvr.display_client.OSGFileStimulusSlave(dsc, osg_file_desc)
+        osgslaves[name] = stimklass(dsc, osg_file_desc)
 
     # setup camera position
     for name in VR_PANELS:
@@ -106,30 +170,26 @@ def doit(h5_file, fmf_fname, obj_id, tmpdir, outdir, calibration, tfix, framenum
                                 "%s%s_%s%s" % (obj_id,
                                                sml,
                                                "_".join(VR_PANELS),
-                                               "" if fmf_fname.endswith(".fmf") else "_gopro")
+                                               "" if (not fmf_fname or fmf_fname.endswith(".fmf")) else "_gopro")
     )
-
-    query = "obj_id == %d" % obj_id
-    valid = trajectories.readWhere(query)
-
-    starts = trajectory_start_times.readWhere(query)
-    start = starts['first_timestamp_secs'][0] + (starts['first_timestamp_nsecs'][0]*1e-9)
 
     timestamps = np.arange(
                         start,
                         start+(len(valid)*dt),
                         dt)
 
-    if fmf_fname.endswith(".fmf"):
-        print "fmf fname", fmf_fname
-        fmf = motmot.FlyMovieFormat.FlyMovieFormat.FlyMovie(fmf_fname)
-        movie_is_rgb = False #fixe
+    if fmf_fname:
+        if fmf_fname.endswith(".fmf"):
+            print "fmf fname", fmf_fname
+            fmf = motmot.FlyMovieFormat.FlyMovieFormat.FlyMovie(fmf_fname)
+            movie_is_rgb = False #fixe
+        else:
+            path,fps,tstart = fmf_fname.split("|")
+            print "move from image frames in %s at %s fps (ts=%s)" % (path,fps,tstart)
+            fmf = analysislib.movie.ImageDirMovie(path,float(fps),float(tstart))
+            movie_is_rgb = fmf.is_rgb
     else:
-        path,fps,tstart = fmf_fname.split("|")
-        print "move from image frames in %s at %s fps (ts=%s)" % (path,fps,tstart)
-        fmf = analysislib.movie.ImageDirMovie(path,float(fps),float(tstart))
-        movie_is_rgb = fmf.is_rgb
-
+        fmf = None
 
     xyz = np.c_[valid['x'],valid['y'],valid['z']]
     if camera:
@@ -140,12 +200,15 @@ def doit(h5_file, fmf_fname, obj_id, tmpdir, outdir, calibration, tfix, framenum
 
     print "trajectory ranges from", timestamps[0], "to", timestamps[-1]
 
-    fmftimestamps = fmf.get_all_timestamps()
+    if fmf is not None:
+        fmftimestamps = fmf.get_all_timestamps()
 
-    print "fmf ranges from", fmftimestamps[0], "to", fmftimestamps[-1]
+        print "fmf ranges from", fmftimestamps[0], "to", fmftimestamps[-1]
 
-    num = 0
-    t0 = fmf.get_frame_at_or_before_timestamp(timestamps[0])[1]
+        num = 0
+        t0 = fmf.get_frame_at_or_before_timestamp(timestamps[0])[1]
+    else:
+        t0 = timestamps[0]
 
     if not sml:
         target_out_w = TARGET_OUT_W*2
@@ -161,33 +224,63 @@ def doit(h5_file, fmf_fname, obj_id, tmpdir, outdir, calibration, tfix, framenum
 
     #define the panels maximum size
     panels = {}
-    panels["movie"] = dict(
-        width = fmf.width,
-        height = fmf.height,
-        device_x0 = MARGIN,
-        device_x1 = target_out_w//(1+len(VR_PANELS)) - MARGIN//2,
-    )
-    for n,name in enumerate(VR_PANELS):
-        panels[name] = dict(
-            width = renderers[name].dsc.width,
-            height = renderers[name].dsc.height,
-            device_x0 = (1+n)*target_out_w//(1+len(VR_PANELS)) + MARGIN//2,
-            device_x1 = (2+n)*target_out_w//(1+len(VR_PANELS)) - MARGIN//2,
+    if fmf is not None:
+        panels["movie"] = dict(
+            width = fmf.width,
+            height = fmf.height,
+            device_x0 = MARGIN,
+            device_x1 = target_out_w//(1+len(VR_PANELS)) - MARGIN//2,
         )
+    if plot:
+        panels["plot"] = dict(
+            width = 533, #same aspect ratio 1024x768
+            height = 400,
+            device_x0 = MARGIN,
+            device_x1 = target_out_w//(1+len(VR_PANELS)) - MARGIN//2,
+        )
+
+    if len(panels) > 0:
+        #FIXME doesnt work for != 1
+        for n,name in enumerate(VR_PANELS):
+            panels[name] = dict(
+                width = renderers[name].dsc.width,
+                height = renderers[name].dsc.height,
+                device_x0 = (1+n)*target_out_w//(1+len(VR_PANELS)) + MARGIN//2,
+                device_x1 = (2+n)*target_out_w//(1+len(VR_PANELS)) - MARGIN//2
+            )
+    else:
+        for n,name in enumerate(VR_PANELS):
+            panels[name] = dict(
+                width = renderers[name].dsc.width,
+                height = renderers[name].dsc.height,
+                device_x0 = (n)*target_out_w//(len(VR_PANELS)) + MARGIN//2,
+                device_x1 = (1+n)*target_out_w//(len(VR_PANELS)) - MARGIN//2,
+            )
     #calculate sizes of the panels that fit
     actual_out_w, actual_out_h = benu.utils.negotiate_panel_size(panels, max_height, MARGIN)
 
     pbar = analysislib.get_progress_bar(str(obj_id), len(timestamps))
 
+    MAXLEN = None
+    xhist = collections.deque(maxlen=MAXLEN)
+    yhist = collections.deque(maxlen=MAXLEN)
+    zhist = collections.deque(maxlen=MAXLEN)
+
+    assert len(valid) == len(xyz)
+
     tfirst = None
-    for n,(t,uv,xyz) in enumerate(zip(timestamps,pixel,xyz)):
+    for n,(t,uv,xyz,(dfidx,dfrow)) in enumerate(zip(timestamps,pixel,xyz,valid.iterrows())):
 
         pbar.update(n)
 
-        try:
-            img,ts = fmf.get_frame_at_or_before_timestamp(t)
-        except ValueError:
-            continue
+        if fmf is None:
+            ts = t
+            img = None
+        else:
+            try:
+                img,ts = fmf.get_frame_at_or_before_timestamp(t)
+            except ValueError:
+                continue
 
         if ts > t0:
             t0 = ts
@@ -196,30 +289,47 @@ def doit(h5_file, fmf_fname, obj_id, tmpdir, outdir, calibration, tfix, framenum
             col,row = uv
             x,y,z = xyz
 
-            if movie_is_rgb:
-                rgb_image = img
-            else:
-                #see fmfcat commit for why this is right
-                rgb_image = cv2.cvtColor(img[:,1:],cv2.COLOR_BAYER_GR2RGB)
-                #and this is wrong?
-                #rgb_image = cv2.cvtColor(img,cv2.COLOR_BAYER_BG2RGB)
+            xhist.append(x)
+            yhist.append(y)
+            zhist.append(z)
+
+            if img is not None:
+                if movie_is_rgb:
+                    rgb_image = img
+                else:
+                    #see fmfcat commit for why this is right
+                    rgb_image = cv2.cvtColor(img[:,1:],cv2.COLOR_BAYER_GR2RGB)
+                    #and this is wrong?
+                    #rgb_image = cv2.cvtColor(img,cv2.COLOR_BAYER_BG2RGB)
 
             imgfname = movie.next_frame()
             canv = benu.benu.Canvas(imgfname,actual_out_w,actual_out_h)
             canv.poly([0,0,actual_out_w,actual_out_w,0],[0,actual_out_h,actual_out_h,0,0], color_rgba=(0,0,0,1))
 
             #do the movie first
-            m = panels["movie"]
-            device_rect = (m["device_x0"], device_y0, m["dw"], m["dh"])
-            user_rect = (0,0,m["width"], m["height"])
-            with canv.set_user_coords(device_rect, user_rect) as _canv:
-                _canv.imshow(rgb_image, 0,0, filter='best' )
-                if (not np.isnan(col)) and (not np.isnan(row)):
-                    _canv.scatter([col], [row], color_rgba=(1,0,0,0.8), radius=6, markeredgewidth=5 )
+            if img is not None:
+                m = panels["movie"]
+                device_rect = (m["device_x0"], device_y0, m["dw"], m["dh"])
+                user_rect = (0,0,m["width"], m["height"])
+                with canv.set_user_coords(device_rect, user_rect) as _canv:
+                    _canv.imshow(rgb_image, 0,0, filter='best' )
+                    if (not np.isnan(col)) and (not np.isnan(row)):
+                        _canv.scatter([col], [row], color_rgba=(1,0,0,0.8), radius=6, markeredgewidth=5 )
+
+            if plot:
+                m = panels["plot"]
+                device_rect = (m["device_x0"], device_y0, m["dw"], m["dh"])
+                user_rect = (0,0,m["width"], m["height"])
+                with canv.set_user_coords(device_rect, user_rect) as _canv:
+                    with _canv.get_figure(m["dw"], m["dh"]) as fig:
+                        analysislib.movie.plot_xyz(fig, movie.frame_number,
+                            xhist, yhist, zhist, x, y, z
+                        )
 
             #do the VR 
             for name in VR_PANELS:
                 renderers[name].set_pose(x=x,y=y,z=z)
+                osgslaves[name].set_state(dfrow)
 
                 fn = os.path.basename(imgfname)
                 myfname = imgfname.replace(fn,name+fn)
@@ -245,16 +355,13 @@ def doit(h5_file, fmf_fname, obj_id, tmpdir, outdir, calibration, tfix, framenum
 if __name__ == "__main__":
     rospy.init_node('osgrender')
 
-    parser = analysislib.args.get_parser("uuid", "h5-file", "idfilt", "outdir", "basedir")
+    parser = analysislib.args.get_parser()
     parser.add_argument(
-        '--movie-file', type=str, required=True,
+        '--movie-file', type=str, default='',
         help='path to movie file (fmf or mp4)')
     parser.add_argument(
         '--calibration', type=str, required=False,
         help='path to camera calibration file')
-    parser.add_argument(
-        '--tfix', type=float, default=0.0,
-        help='time offset to fixup movie')
     parser.add_argument(
         '--tmpdir', type=str, default='/tmp/',
         help='path to temporary directory')
@@ -262,30 +369,29 @@ if __name__ == "__main__":
         '--no-framenumber', action='store_false', dest="framenumber", default="true",
         help='dont render the framenumber on the video')
     parser.add_argument(
+        '--plot', action='store_true',
+        help='plot x,y,z')
+    parser.add_argument(
         '--osgdesc', type=str, default='posts3.osg',
         help='osg file descriptor string')
+    parser.add_argument(
+        '--stimulus', type=str, default='StimulusOSGFile',
+        help='flyvr stimulus name')
 
     argv = rospy.myargv()
     args = parser.parse_args(argv[1:])
 
     if (not args.h5_file) and (not args.uuid):
-        parser.error("Specify a UUID or a H5 file")
+        parser.error("Specify a UUID or an H5 file")
 
     if args.uuid is not None:
         if len(args.uuid) > 1:
             parser.error("Only one uuid supported for making movies")
-
         uuid = args.uuid[0]
-
-        fm = autodata.files.FileModel(basedir=args.basedir)
-        fm.select_uuid(uuid)
-        h5_file = fm.get_file_model("simple_flydra.h5").fullpath
     else:
         uuid = ''
 
-        h5_file = args.h5_file
-
-    outdir = args.outdir if args.outdir is not None else strawlab.constants.get_move_dir(uuid)
+    outdir = args.outdir if args.outdir is not None else strawlab.constants.get_movie_dir(uuid)
 
     try:
         assert len(args.idfilt) == 1
@@ -296,23 +402,13 @@ if __name__ == "__main__":
     if obj_id is None:
         parser.error("You must specify --idfilt with a single obj_id")
 
-    doit(h5_file, args.movie_file, obj_id, args.tmpdir, outdir, args.calibration, args.tfix, args.framenumber,
-            '_sml',
-            args.osgdesc)
-
-##examples
-# LISA
-#    obj_ids = [738,3065,1607,1925,1586]
-#    obj_ids = [738]
-#    uuid   = 'be130ece9db611e2b8fe6c626d3a008a'
-#    outdir = '/mnt/strawarchive/John/post-for-lisa-ist/'
-#    outdir = os.getcwd()
-#python movie-osgfile-virtualworld.py --uuid be130ece9db611e2b8fe6c626d3a008a --movie-file /mnt/strawscience/movies/Flycave/be130ece9db611e2b8fe6c626d3a008a/Basler_21266086/738.fmf --calibration /home/strawlab/ros-flycave.electric.boost1.46/flycave/calibration/feb2013/colormoviecamcalib.bag --idfilt 738 --outdir /tmp/
-
-##Colored BOX GoPro
-#python movie-osgfile-virtualworld.py --uuid 39665d18d81011e292be6c626d3a008a --movie-file '/mnt/ssd/tmp/GOPRO/|60.0|1371559150.51' --idfilt 16 --outdir . --tmpdir /mnt/ssd/tmp/ --osgdesc L.osgt/0.0,0.0,0.29/0.1,0.1,0.3
-
-##Colored BOX Movie cam
-#python movie-osgfile-virtualworld.py --uuid 39665d18d81011e292be6c626d3a008a --movie-file /mnt/strawscience/movies/Flycave/39665d18d81011e292be6c626d3a008a/Basler_21266086/16.fmf --calibration /home/strawlab/ros-flycave.electric.boost1.46/flycave/calibration/feb2013/colormoviecamcalib.bag --idfilt 16 --outdir . --tmpdir /mnt/ssd/tmp/ --osgdesc L.osgt/0.0,0.0,0.29/0.1,0.1,0.3
-
+    doit(args,
+         args.movie_file,
+         obj_id, args.tmpdir,
+         outdir, args.calibration, args.framenumber,
+         '_sml',
+         args.stimulus,
+         args.osgdesc,
+         args.plot
+    )
 
