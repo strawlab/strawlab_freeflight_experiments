bce700e4a7acef2f12248294e0db9bd9d167c183
John Stowers <john.stowers@gmail.com>
2013-07-31 12:59:25 +0200

    rotation-analysis: support flycube csv files

--- scripts/rotation-analysis.py
+++ scripts/rotation-analysis.py
@@ -36,7 +36,7 @@ if __name__=='__main__':
                             rotation.Logger,
                             "ratio","rotation_rate",
     )
-    combine.add_from_args(args, "rotation.csv", frames_before=0)
+    combine.add_from_args(args, "rotation*.csv", frames_before=0)
 
     fname = combine.fname
     results,dt = combine.get_results()
