17fa27050e5cc21069d2d21ce7f6545572181833
John Stowers <john.stowers@gmail.com>
2013-10-10 10:41:04 +0200

    analysislib: move test data in tree

--- test/test_datastore.py
+++ test/test_datastore.py
@@ -5,7 +5,6 @@ import numpy as np
 import unittest
 import collections
 import tempfile
-import tarfile
 import shutil
 
 import roslib
@@ -18,20 +17,15 @@ class TestDataStore(unittest.TestCase):
 
     def setUp(self):
         self.uuid = '0'*32
-        self.tdir = tempfile.mkdtemp()
-        self.ddir = os.path.join(self.tdir, self.uuid)
-        self.pdir = os.path.join(self.tdir, 'plots')
-        os.makedirs(self.ddir); os.makedirs(self.pdir)
+        ddir = os.path.join(
+                    roslib.packages.get_pkg_dir('strawlab_freeflight_experiments'),
+                   'test','data'
+        )
+        self.ddir = os.path.join(ddir, self.uuid)
+        self.pdir = os.path.join(ddir, 'plots')
         #make autodata look in the tempdir for file
-        os.environ['FLYDRA_AUTODATA_BASEDIR'] = self.tdir
+        os.environ['FLYDRA_AUTODATA_BASEDIR'] = ddir
         os.environ['FLYDRA_AUTODATA_PLOTDIR'] = self.pdir
-
-        #extract out trajectories there
-        testdata = os.path.join(roslib.packages.get_pkg_dir('strawlab_freeflight_experiments'),
-                           'test','20131004_161625.tar.bz2')
-
-        t = tarfile.open(testdata,'r:bz2')
-        t.extractall(self.ddir)
 
         self.combine = autil.get_combiner("rotation.csv")
         self.combine.add_from_uuid(self.uuid)
@@ -54,7 +48,6 @@ class TestDataStore(unittest.TestCase):
     def tearDown(self):
         del os.environ['FLYDRA_AUTODATA_BASEDIR']
         del os.environ['FLYDRA_AUTODATA_PLOTDIR']
-        shutil.rmtree(self.tdir)
 
 if __name__=='__main__':
     unittest.main()
