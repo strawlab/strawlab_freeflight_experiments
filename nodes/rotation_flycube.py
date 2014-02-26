96a02dd1a6633ac4d12e8a303c058e8cd39acc52
John Stowers <john.stowers@gmail.com>
2014-02-26 13:37:49 +0100

    rotation_flycube: merge in flycave rotation, adding support for replay experiments

--- nodes/rotation_flycube.py
+++ nodes/rotation_flycube.py
@@ -17,11 +17,11 @@ import flyvr.display_client as display_client
 from std_msgs.msg import UInt32, Bool, Float32, String
 from geometry_msgs.msg import Vector3, Pose
 from ros_flydra.msg import flydra_mainbrain_super_packet
-import rospkg
 
 import flyflypath.model
 import flyflypath.transform
 import nodelib.log
+import strawlab_freeflight_experiments.replay as sfe_replay
 
 pkg_dir = roslib.packages.get_pkg_dir(PACKAGE)
 
@@ -70,13 +70,25 @@ TAU= 2*PI
 
 MAX_ROTATION_RATE = 1.5
 
+REPLAY_ARGS_ROTATION = dict(filename=os.path.join(pkg_dir,
+                                              "data","replay_experiments",
+                                              "9b97392ebb1611e2a7e46c626d3a008a_9.df"),
+                            colname="rotation_rate")
+REPLAY_ARGS_Z = dict(filename=os.path.join(pkg_dir,
+                                              "data","replay_experiments",
+                                              "9b97392ebb1611e2a7e46c626d3a008a_9.df"),
+                     colname="v_offset_rate")
+
 #CONDITION = "cylinder_image/
 #             svg_path(if omitted target = 0,0)/
 #             gain/
-#             radius_when_locked(-ve disables cx,xy changing)/
+#             radius_when_locked(+ve = centre of cylinder is locked to the fly)/
 #             advance_threshold(m)/
 #             z_gain,
 #             z_target"
+#
+# if you set nan for either gain then you get a replay experiment on the
+# corresponding bias term
 #
 CONDITIONS = [
               "checkerboard16.png/infinity05.svg/+0.2/-10.0/0.1/0.30/0.18",
@@ -100,23 +112,23 @@ class Node(object):
         self._pub_stim_mode = display_client.DisplayServerProxy.set_stimulus_mode(
             'StimulusCylinder')
 
-        self.rotation_pub = rospy.Publisher(TOPIC_CYL_ROTATION, Float32, latch=True, tcp_nodelay=True)
-        self.rotation_velocity_pub = rospy.Publisher(TOPIC_CYL_ROTATION_RATE, Float32, latch=True, tcp_nodelay=True)
-        self.v_offset_value_pub = rospy.Publisher(TOPIC_CYL_V_OFFSET_VALUE, Float32, latch=True, tcp_nodelay=True)
-        self.v_offset_rate_pub = rospy.Publisher(TOPIC_CYL_V_OFFSET_RATE, Float32, latch=True, tcp_nodelay=True)
-        self.image_pub = rospy.Publisher(TOPIC_CYL_IMAGE, String, latch=True, tcp_nodelay=True)
-        self.cyl_centre_pub = rospy.Publisher(TOPIC_CYL_CENTRE, Vector3, latch=True, tcp_nodelay=True)
-        self.cyl_radius_pub = rospy.Publisher(TOPIC_CYL_RADIUS, Float32, latch=True, tcp_nodelay=True)
-        self.cyl_height_pub = rospy.Publisher(TOPIC_CYL_HEIGHT, Float32, latch=True, tcp_nodelay=True)
+        self.pub_rotation = rospy.Publisher(TOPIC_CYL_ROTATION, Float32, latch=True, tcp_nodelay=True)
+        self.pub_rotation_velocity = rospy.Publisher(TOPIC_CYL_ROTATION_RATE, Float32, latch=True, tcp_nodelay=True)
+        self.pub_v_offset_value = rospy.Publisher(TOPIC_CYL_V_OFFSET_VALUE, Float32, latch=True, tcp_nodelay=True)
+        self.pub_v_offset_rate = rospy.Publisher(TOPIC_CYL_V_OFFSET_RATE, Float32, latch=True, tcp_nodelay=True)
+        self.pub_image = rospy.Publisher(TOPIC_CYL_IMAGE, String, latch=True, tcp_nodelay=True)
+        self.pub_cyl_centre = rospy.Publisher(TOPIC_CYL_CENTRE, Vector3, latch=True, tcp_nodelay=True)
+        self.pub_cyl_radius = rospy.Publisher(TOPIC_CYL_RADIUS, Float32, latch=True, tcp_nodelay=True)
+        self.pub_cyl_height = rospy.Publisher(TOPIC_CYL_HEIGHT, Float32, latch=True, tcp_nodelay=True)
 
-        self.pushover_pub = rospy.Publisher('note', String)
-        self.save_pub = rospy.Publisher('save_object', UInt32)
+        self.pub_pushover = rospy.Publisher('note', String)
+        self.pub_save = rospy.Publisher('save_object', UInt32)
 
-        self.rotation_pub.publish(0)
-        self.v_offset_value_pub.publish(0)
+        self.pub_rotation.publish(0)
+        self.pub_v_offset_value.publish(0)
 
-        self.lock_object = rospy.Publisher('lock_object', UInt32, latch=True, tcp_nodelay=True)
-        self.lock_object.publish(IMPOSSIBLE_OBJ_ID)
+        self.pub_lock_object = rospy.Publisher('lock_object', UInt32, latch=True, tcp_nodelay=True)
+        self.pub_lock_object.publish(IMPOSSIBLE_OBJ_ID)
 
         self.log = Logger(wait=wait_for_flydra, use_tmpdir=use_tmpdir, continue_existing=continue_existing)
 
@@ -138,10 +150,11 @@ class Node(object):
 
             self.ratio_total = 0
 
-        #start criteria for experiment
-        self.x0 = 0
-        self.y0 = 0
+            self.replay_rotation = sfe_replay.ReplayStimulus(**REPLAY_ARGS_ROTATION)
+            self.replay_z = sfe_replay.ReplayStimulus(**REPLAY_ARGS_Z)
 
+        #start criteria for experiment
+        self.x0 = self.y0 = 0
         #target (for moving points)
         self.trg_x = self.trg_y = 0.0
 
@@ -159,6 +172,13 @@ class Node(object):
                          flydra_mainbrain_super_packet,
                          self.on_flydra_mainbrain_super_packets)
 
+    @property
+    def is_replay_experiment_rotation(self):
+        return np.isnan(self.p_const)
+    @property
+    def is_replay_experiment_z(self):
+        return np.isnan(self.v_gain)
+
     def switch_conditions(self,event,force=''):
         if force:
             self.condition = force
@@ -169,7 +189,6 @@ class Node(object):
         self.log.condition = self.condition
 
         self.drop_lock_on()
-        rospy.loginfo('SWITCH: switch conditions')
 
         img,svg,p,rad,advance,v_gain,z_target = self.condition.split('/')
         self.img_fn = str(img)
@@ -188,19 +207,16 @@ class Node(object):
         else:
             self.svg_fn = ''
 
-        if self.rad_locked < 0:
-            #HACK
-            self.cyl_height_pub.publish(-20*self.rad_locked)
-        else:
-            self.cyl_height_pub.publish(1.0)
-
+        #HACK
+        self.pub_cyl_height.publish(np.abs(5*self.rad_locked))
+        
         rospy.loginfo('condition: %s (p=%.1f, svg=%s, rad locked=%.1f advance=%.1fpx)' % (self.condition,self.p_const,os.path.basename(self.svg_fn),self.rad_locked,self.advance_px))
 
     def get_v_rate(self,fly_z):
         return self.v_gain*(fly_z-self.z_target)
 
     def get_rotation_velocity_vector(self,fly_x,fly_y,fly_z, fly_vx, fly_vy, fly_vz):
-        if self.svg_fn:
+        if self.svg_fn and (not self.is_replay_experiment_rotation):
             with self.trackinglock:
                 px,py = XFORM.xy_to_pxpy(fly_x,fly_y)
                 segment = self.model.connect_to_moving_point(p=None, px=px,py=py)
@@ -210,6 +226,10 @@ class Node(object):
                     self.ratio_total += ADVANCE_RATIO
         else:
             self.trg_x = self.trg_y = 0.0
+
+        #return early if this is a replay experiment
+        if self.is_replay_experiment_rotation:
+            return self.replay_rotation.next(), self.trg_x,self.trg_y
 
         dpos = np.array((self.trg_x-fly_x,self.trg_y-fly_y))
         vel  = np.array((fly_vx, fly_vy))
@@ -297,23 +317,23 @@ class Node(object):
                 v_rate = self.get_v_rate(fly_z)
 
                 px,py = XFORM.xy_to_pxpy(fly_x,fly_y)
-                self.src_pub.publish(px,py,0)
+                self.src_pub.publish(px,py,fly_z)
                 trg_px, trg_py = XFORM.xy_to_pxpy(trg_x,trg_y)
-                self.trg_pub.publish(trg_px,trg_py,0)
+                self.trg_pub.publish(trg_px,trg_py,self.z_target)
 
                 self.log.cyl_x = fly_x; self.log.cyl_y = fly_y;
                 self.log.trg_x = trg_x; self.log.trg_y = trg_y; self.log.trg_z = self.z_target
 
                 self.log.rotation_rate = rate
-                self.rotation_velocity_pub.publish(rate)
+                self.pub_rotation_velocity.publish(rate)
 
                 self.log.v_offset_rate = v_rate
-                self.v_offset_rate_pub.publish(v_rate)
+                self.pub_v_offset_rate.publish(v_rate)
 
                 if self.rad_locked > 0:
-                    self.cyl_centre_pub.publish(fly_x,fly_y,0)
+                    self.pub_cyl_centre.publish(fly_x,fly_y,0)
                 else:
-                    self.cyl_centre_pub.publish(0,0,0)
+                    self.pub_cyl_centre.publish(0,0,0)
 
                 self.log.framenumber = framenumber
 
@@ -351,7 +371,7 @@ class Node(object):
 
     def update(self):
         self.log.update()
-        self.lock_object.publish( self.log.lock_object )
+        self.pub_lock_object.publish( self.log.lock_object )
 
     def lock_on(self,obj,framenumber):
         with self.trackinglock:
@@ -375,9 +395,12 @@ class Node(object):
 
             self.ratio_total = 0
 
-        self.image_pub.publish( self.img_fn )
+            self.replay_rotation.reset()
+            self.replay_z.reset()
 
-        self.cyl_radius_pub.publish(np.abs(self.rad_locked))
+        self.pub_image.publish( self.img_fn )
+
+        self.pub_cyl_radius.publish(np.abs(self.rad_locked))
 
         self.update()
 
@@ -402,16 +425,16 @@ class Node(object):
             self.log.cyl_x = 0
             self.log.cyl_y = 0
 
-        self.image_pub.publish(GRAY_FN)
-        self.rotation_velocity_pub.publish(0)
+        self.pub_image.publish(GRAY_FN)
+        self.pub_rotation_velocity.publish(0)
 
-        self.cyl_radius_pub.publish(0.5)
-        self.cyl_centre_pub.publish(0,0,0)
+        self.pub_cyl_radius.publish(0.5)
+        self.pub_cyl_centre.publish(0,0,0)
 
         if (self.ratio_total > 2) and (old_id is not None):
             if self.condition in COOL_CONDITIONS:
-                self.pushover_pub.publish("Fly %s flew %.1f loops (in %.1fs)" % (old_id, self.ratio_total, dt))
-                self.save_pub.publish(old_id)
+                self.pub_pushover.publish("Fly %s flew %.1f loops (in %.1fs)" % (old_id, self.ratio_total, dt))
+                self.pub_save.publish(old_id)
 
         self.update()
 
