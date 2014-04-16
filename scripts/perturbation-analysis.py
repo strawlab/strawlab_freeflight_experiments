5c345714073514003acc0b7709f5edbf6ffd8c9e
John Stowers <john.stowers@gmail.com>
2014-04-16 15:04:42 +0200

    perturb-analysis: rewrite
    * show more things as timeseries
    * show the perturbation
    * plot trajectories while pertubed

--- scripts/perturbation-analysis.py
+++ scripts/perturbation-analysis.py
@@ -24,6 +24,8 @@ import analysislib.plots as aplt
 import analysislib.curvature as curve
 import analysislib.util as autil
 
+import strawlab_freeflight_experiments.perturb as sfe_perturb
+
 if __name__=='__main__':
     parser = analysislib.args.get_parser()
 
@@ -53,54 +55,131 @@ if __name__=='__main__':
     aplt.save_args(combine, args)
     aplt.save_results(combine, args)
 
-    step_conds = [c for c in results if 'step' in c]
+    arena = aplt.get_arena_from_args(args)
+    (xmin,xmax, ymin,ymax, zmin,zmax) = arena.get_bounds()
 
+    TO_PLOT = {"dtheta":{"ylim":(-10,10)},
+               "z":{"ylim":(zmin,zmax)},
+               "velocity":{},
+               "rotation_rate":{},
+    }
+
+    step_conds = [c for c in results if 'step' in c]
     for cond in step_conds:
 
-        series = {}
+        step_desc = cond.split("/")[-1]
+        step_obj = sfe_perturb.get_perturb_class(step_desc)(step_desc)
 
         r = results[cond]
+
+        dfs = {}
+        idxs = {}
+        lidxs = {}
+
         for _df,(x0,y0,obj_id,framenumber0,time0) in zip(r['df'], r['start_obj_ids']):
+
             df = _df.fillna(method='ffill')
 
             #find the start of the perturbation (where perturb_progress == 0)
             z = np.where(df['perturb_progress'].values == 0)
             if len(z[0]):
-                lidx = z[0][0]
-                df['align'] = np.array(range(len(df)), dtype=int) - lidx
-                series["%d" % obj_id] = pd.Series(df['dtheta'].values, index=df['align'].values)
+                fidx = z[0][0]
 
-        if series:
-            name = combine.get_plot_filename('ts_%s' % aplt.get_safe_filename(cond))
+                #find the index of the last perturbation (max -1)
+                l = np.where(df['perturb_progress'].values == df['perturb_progress'].max())
+                lidx = l[0][0]
 
+                #ensure we get a unique obj_id for later grouping. That is not necessarily
+                #guarenteed because obj_ids may be in multiple conditions, so if need be
+                #create a new one
+                if obj_id in dfs:
+                    obj_id = int(time.time()*1e6)
+                df['obj_id'] = obj_id
+
+                t = time0 + (np.arange(0,len(df),dtype=float) * dt)
+                df['time'] = t
+                df['talign'] = t - t[fidx]
+
+                df['align'] = np.array(range(len(df)), dtype=int) - fidx
+
+                dfs[obj_id] = df
+                idxs[obj_id] = fidx
+                lidxs[obj_id] = lidx
+
+        if dfs:
+            pool = pd.concat(dfs.values(),join="outer",axis=0)
+
+            for to_plot in TO_PLOT:
+                grouped_oid = pool.groupby('obj_id')
+
+                #plot timeseries
+                name = combine.get_plot_filename('ts_%s_%s' % (to_plot,aplt.get_safe_filename(cond)))
+                with aplt.mpl_fig(name,args,figsize=(8,6)) as fig:
+
+                    ax = fig.add_subplot(1,1,1)
+                    ax2 = ax.twinx()
+
+                    ax.set_title("%s" % cond, fontsize=12)
+
+                    ax.text(0.01, 0.99, #top left
+                            "n=%d" % len(dfs),
+                            fontsize=10,
+                            horizontalalignment='left',
+                            verticalalignment='top',
+                            transform=ax.transAxes,
+                            color='k')
+
+                    for oid,_df in grouped_oid:
+
+                        t = _df['talign'].values
+                        v = _df[to_plot].values
+
+                        ax.plot(t, v, 'k-', alpha=0.1)
+                        ax.plot(t[-1], v[-1], 'ko', alpha=0.4, markersize=2)
+
+                    grouped = pool.groupby('align')
+                    m = grouped.mean()
+                    ax.plot(m['talign'].values, m[to_plot].values, 'r-', lw=2.0, alpha=0.8, label="mean")
+
+                    ax.set_ylabel(to_plot)
+                    ax.set_xlabel('t (s)')
+
+                    #plot from one second before to one second after
+                    t0,t1 = step_obj.get_time_limits()
+                    t0 -= 1; t1 += 1
+
+                    ax2.set_ylabel(str(step_obj),color='b')
+
+                    t,v = step_obj.get_perturb_vs_time(t0,t1)
+                    ax2.plot(t,v, 'b-')
+                    v0,v1 = step_obj.get_value_limits()
+                    ax2.set_ylim(min(-0.1,1.2*v0),1.2*v1)
+                    for tl in ax2.get_yticklabels():
+                        tl.set_color('b')
+
+                    ax.set_xlim(t0,t1)
+
+                    if "ylim" in TO_PLOT[to_plot]:
+                        ax.set_ylim(*TO_PLOT[to_plot]["ylim"])
+
+                    fig.canvas.mpl_connect('draw_event', aplt.autowrap_text)
+
+            #plot trajectories while under perturbation
+            name = combine.get_plot_filename('xy_%s' % aplt.get_safe_filename(cond))
             with aplt.mpl_fig(name,args,figsize=(8,6)) as fig:
-
-                fig.suptitle(cond, fontsize=12)
                 ax = fig.add_subplot(1,1,1)
+                ax.set_title("%s" % cond, fontsize=12)
 
-                for s in series.itervalues():
+                for oid,_df in grouped_oid:
+                    pdf = _df.iloc[idxs[oid]:lidxs[oid]]
+                    xv = pdf['x'].values
+                    yv = pdf['y'].values
+                    ax.plot( xv, yv, 'k-', lw=1.0, alpha=0.5, rasterized=aplt.RASTERIZE )
 
-                    ax.plot(s.index.values, s.values, 'k-', alpha=0.1)
-                    ax.plot(s.index.values[-1], s.values[-1], 'ko', alpha=0.4)
-
-
-                ax.set_ylabel('dtheta')
-                ax.set_xlabel('framenumber')
-                ax.set_ylim(-10,10)
-                ax.set_xlim(-100,1000)
-
-                df = pd.DataFrame(series)
-                means = df.mean(1)
-
-                t = means.index.values
-
-                v = means.values
-                std = df.std(1).values
-
-                ax.plot(t, v, 'r-', lw=2.0, alpha=0.8, label="mean")
-                #ax.fill_between(t, v+std, v-std, facecolor='red', alpha=0.1)
+                aplt.layout_trajectory_plots(ax, arena, in3d=False)
 
                 fig.canvas.mpl_connect('draw_event', aplt.autowrap_text)
+
 
     if args.show:
         aplt.show_plots()
