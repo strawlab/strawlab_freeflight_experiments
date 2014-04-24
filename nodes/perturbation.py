3ba916e37343bd450a6326697fc0fe2c81631702
John Stowers <john.stowers@gmail.com>
2014-04-24 18:13:29 +0200

    perturbation: more debugging

--- nodes/perturbation.py
+++ nodes/perturbation.py
@@ -250,7 +250,7 @@ class Node(object):
                                              fly_x, fly_y, fly_z, fly_vx, fly_vy, fly_vz,
                                              now, framenumber, currently_locked_obj_id)
 
-                rospy.logdebug('perturbation progress: %s' % self.perturber.progress)
+                print 'perturbation progress: %s' % self.perturber.progress
 
                 if finished:
                     self.drop_lock_on(blacklist=True)
@@ -447,7 +447,7 @@ class Node(object):
             now = rospy.get_time()
             dt = now - self.first_seen_time
 
-            rospy.loginfo('dropping locked object %s (tracked for %.1f)' % (old_id, dt))
+            rospy.loginfo('dropping locked object %s (tracked for %.1f, %.1f loops)' % (old_id, dt, self.ratio_total))
 
             self.currently_locked_obj_id = None
 
