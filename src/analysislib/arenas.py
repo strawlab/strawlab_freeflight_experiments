f42f478b381688c9a9d73363fcb0aaa2ded897d6
John Stowers <john.stowers@gmail.com>
2014-04-25 15:04:32 +0200

    analysislib: move arena 3d plotting to arena class

--- src/analysislib/arenas.py
+++ src/analysislib/arenas.py
@@ -10,6 +10,10 @@ class ArenaBase(object):
     def get_ztick_locations(self):
         # override to specify tick locations, otherwise, auto-determined
         return None
+    def plot_mpl_line_2d(self, ax, *args, **kwargs):
+        pass
+    def plot_mpl_3d(self, ax, *args, **kwargs):
+        pass
 
 class FlyCaveCylinder(ArenaBase):
     def __init__(self,radius=0.5,height=1.0):
@@ -19,6 +23,13 @@ class FlyCaveCylinder(ArenaBase):
         rad = self.radius
         theta = np.linspace(0, 2*np.pi, 100)
         return ax.plot( rad*np.cos(theta), rad*np.sin(theta), *args, **kwargs)
+    def plot_mpl_3d(self,ax,*args,**kwargs):
+        rad = self.radius
+        theta = np.linspace(0, 2*np.pi, 100)
+        ax.plot(rad*np.cos(theta), rad*np.sin(theta), np.zeros_like(theta),
+                *args, **kwargs)
+        ax.plot(rad*np.cos(theta), rad*np.sin(theta), np.ones_like(theta),
+                *args, **kwargs)
     def get_bounds(self):
         ''' returns (xmin, xmax, ymin, ymax, zmin, zmax)'''
         return (-self.radius, self.radius, -self.radius, self.radius, 0, self.height)
@@ -40,6 +51,26 @@ class FlyCube(ArenaBase):
         xs = [-x, -x, x,  x, -x]
         ys = [-y,  y, y, -y, -y]
         return ax.plot( xs, ys, *args, **kwargs)
+    def plot_mpl_3d(self,ax,*args,**kwargs):
+        x0 = self.xdim/2.0
+        y0 = self.ydim/2.0
+        z1 = self.zdim
+        maxv = np.max(abs(np.array([x0,y0,z1])))
+
+        x = [-x0, -x0, x0,  x0, -x0]
+        y = [-y0,  y0, y0, -y0, -y0]
+        ax.plot(x, y, np.zeros_like(x),
+                *args, **kwargs)
+        ax.plot( x, y, z1*np.ones_like(x),
+                *args, **kwargs)
+
+        # a couple points to fix the aspect ratio correctly
+        ax.plot( [-maxv, maxv],
+                 [-maxv, maxv],
+                 [-maxv, maxv],
+                 'w.',
+                 alpha=0.0001,
+                 markersize=0.0001 )
     def get_bounds(self):
         ''' returns (xmin, xmax, ymin, ymax, zmin, zmax)'''
         x = self.xdim/2.0
