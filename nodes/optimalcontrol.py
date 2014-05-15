d0cfae6e751366d267d9b14e2f5aa2630e35b1d0
John Stowers <john.stowers@gmail.com>
2014-05-15 16:38:22 +0200

    mpc: add first version of controller

---
+++ nodes/optimalcontrol.py
@@ -0,0 +1,382 @@
+#!/usr/bin/env python
+
+import math
+import numpy as np
+import threading
+import argparse
+import os.path
+
+from profilehooks import timecall
+
+import roslib
+import roslib.packages
+roslib.load_manifest('strawlab_freeflight_experiments')
+pkg_dir = roslib.packages.get_pkg_dir('strawlab_freeflight_experiments')
+
+import rospy
+import flyvr.display_client as display_client
+from std_msgs.msg import UInt32, Bool, Float32, String
+from geometry_msgs.msg import Vector3, Pose
+from ros_flydra.msg import flydra_mainbrain_super_packet
+
+import flyflypath.model
+import flyflypath.transform
+import nodelib.log
+
+from strawlab_freeflight_experiments.topics import *
+from strawlab_freeflight_experiments.controllers import MPC
+
+CONTROL_RATE        = 80.0      #Hz
+SWITCH_MODE_TIME    = 5.0*60    #alternate between control and static (i.e. experimental control) seconds
+
+FLY_DIST_CHECK_TIME = 5.0
+FLY_DIST_MIN_DIST   = 0.2
+
+START_RADIUS    = 0.35
+START_ZDIST     = 0.4
+START_Z         = 0.5
+
+# z range for fly tracking (dropped outside)
+Z_MINIMUM = 0.00
+Z_MAXIMUM = 0.95
+
+GRAY_FN = "gray.png"
+
+TIMEOUT             = 0.5
+IMPOSSIBLE_OBJ_ID   = 0
+
+PI = np.pi
+TAU= 2*PI
+
+MAX_ROTATION_RATE = 1.5
+
+#CONDITION = "cylinder_image/
+#             svg_path(if omitted target = 0,0)/
+#             gain/
+#             radius_when_locked(+ve = centre of cylinder is locked to the fly)/
+#             advance_threshold(m)/
+#             z_gain"
+#
+CONDITIONS = [
+              "checkerboard16.png//nan/-10.0/nan/0.20",
+              "gray.png//nan/-10.0/nan/0.20",
+]
+START_CONDITION = CONDITIONS[0]
+#If there is a considerable flight in these conditions then a pushover
+#message is sent and a video recorded
+COOL_CONDITIONS = set()
+
+XFORM = flyflypath.transform.SVGTransform()
+
+class Logger(nodelib.log.CsvLogger):
+    STATE = ("rotation_rate","trg_x","trg_y","trg_z","cyl_x","cyl_y","cyl_r","ratio","v_offset_rate")
+
+class Node(object):
+    def __init__(self, wait_for_flydra, use_tmpdir, continue_existing):
+
+        self._pub_stim_mode = display_client.DisplayServerProxy.set_stimulus_mode(
+            'StimulusCylinder')
+
+        self.pub_rotation = rospy.Publisher(TOPIC_CYL_ROTATION, Float32, latch=True, tcp_nodelay=True)
+        self.pub_rotation_velocity = rospy.Publisher(TOPIC_CYL_ROTATION_RATE, Float32, latch=True, tcp_nodelay=True)
+        self.pub_v_offset_value = rospy.Publisher(TOPIC_CYL_V_OFFSET_VALUE, Float32, latch=True, tcp_nodelay=True)
+        self.pub_v_offset_rate = rospy.Publisher(TOPIC_CYL_V_OFFSET_RATE, Float32, latch=True, tcp_nodelay=True)
+        self.pub_image = rospy.Publisher(TOPIC_CYL_IMAGE, String, latch=True, tcp_nodelay=True)
+        self.pub_cyl_centre = rospy.Publisher(TOPIC_CYL_CENTRE, Vector3, latch=True, tcp_nodelay=True)
+        self.pub_cyl_radius = rospy.Publisher(TOPIC_CYL_RADIUS, Float32, latch=True, tcp_nodelay=True)
+        self.pub_cyl_height = rospy.Publisher(TOPIC_CYL_HEIGHT, Float32, latch=True, tcp_nodelay=True)
+
+        self.pub_pushover = rospy.Publisher('note', String)
+        self.pub_save = rospy.Publisher('save_object', UInt32)
+
+        self.pub_rotation.publish(0)
+        self.pub_v_offset_value.publish(0)
+
+        self.pub_lock_object = rospy.Publisher('lock_object', UInt32, latch=True, tcp_nodelay=True)
+        self.pub_lock_object.publish(IMPOSSIBLE_OBJ_ID)
+
+        self.log = Logger(wait=wait_for_flydra, use_tmpdir=use_tmpdir, continue_existing=continue_existing)
+
+        #setup the MPC controller
+        self.controllock = threading.Lock()
+        with self.controllock:
+            self.control = MPC.MPC()
+            self.control.reset()
+
+        #protect the tracked id and fly position between the time syncronous main loop and the asyn
+        #tracking/lockon/off updates
+        self.trackinglock = threading.Lock()
+        with self.trackinglock:
+            self.currently_locked_obj_id = None
+            self.fly = Vector3()
+            self.framenumber = 0
+            now = rospy.get_time()
+            self.first_seen_time = now
+            self.last_seen_time = now
+            self.last_fly_x = self.fly.x; self.last_fly_y = self.fly.y; self.last_fly_z = self.fly.z;
+            self.last_check_flying_time = now
+            self.fly_dist = 0
+
+            self.ratio_total = 0
+
+        #start criteria for experiment
+        self.x0 = self.y0 = 0
+        #target (for moving points)
+        self.trg_x = self.trg_y = 0.0
+
+        self.svg_pub = rospy.Publisher("svg_filename", String, latch=True)
+        self.src_pub = rospy.Publisher("source", Vector3)
+        self.trg_pub = rospy.Publisher("target", Vector3)
+        self.ack_pub = rospy.Publisher("active", Bool)
+
+        self.switch_conditions(None,force=START_CONDITION)
+
+        self.timer = rospy.Timer(rospy.Duration(SWITCH_MODE_TIME),
+                                  self.switch_conditions)
+
+        rospy.Subscriber("flydra_mainbrain/super_packets",
+                         flydra_mainbrain_super_packet,
+                         self.on_flydra_mainbrain_super_packets)
+
+    @timecall(stats=1000, immediate=False)
+    def do_control(self):
+        self.control.run_control()
+
+    @timecall(stats=1000, immediate=False)
+    def do_update_ekf(self, x,y):
+        self.control.run_ekf(None,x,y)
+
+    @timecall(stats=1000, immediate=False)
+    def do_calculate_input(self):
+        self.control.run_calculate_input()
+
+    def switch_conditions(self,event,force=''):
+        if force:
+            self.condition = force
+        else:
+            i = CONDITIONS.index(self.condition)
+            j = (i+1) % len(CONDITIONS)
+            self.condition = CONDITIONS[j]
+        self.log.condition = self.condition
+
+        self.drop_lock_on()
+
+        img,svg,p,rad,advance,v_gain = self.condition.split('/')
+        self.img_fn = str(img)
+        self.p_const = float(p)
+        self.v_gain = float(v_gain)
+        self.rad_locked = float(rad)
+        self.z_target = 0.7
+        self.svg_fn = str(svg)
+        self.svg_pub.publish(self.svg_fn)
+
+
+        self.log.cyl_r = self.rad_locked
+
+        #HACK
+        self.pub_cyl_height.publish(np.abs(5*self.rad_locked))
+        
+        rospy.loginfo('condition: %s (p=%.1f, svg=%s, rad locked=%.1f)' % (self.condition,self.p_const,os.path.basename(self.svg_fn),self.rad_locked))
+
+    def get_v_rate(self,fly_z):
+        return self.v_gain*(fly_z-self.z_target)
+
+    def run(self):
+        rospy.loginfo('running stimulus')
+        #stick close to matlab/simulink, so run this loop at the fastest
+        #rate (2.5ms) and do our own scheduling
+        r = rospy.Rate(1000/2.5)
+        i = 0
+        while not rospy.is_shutdown():
+            with self.trackinglock:
+                currently_locked_obj_id = self.currently_locked_obj_id
+                fly_x = self.fly.x
+                fly_y = self.fly.y
+                fly_z = self.fly.z
+
+            while True:
+                i += 1
+                now = rospy.get_time()
+                if currently_locked_obj_id is not None:
+                    #5ms
+                    if (i % 2) == 0:
+                        with self.controllock:
+                            if self.control.ekf_enabled:
+                                self.do_update_ekf(fly_x, fly_y)
+
+                            #FIXME: log stuff here for martin
+
+                    #12.5ms
+                    if (i % 5) == 0:
+                        with self.controllock:
+                            self.do_calculate_input()
+
+                        #lets do the vertical control here too,
+                        #at the same rate, 80Hz, as old
+                        v_rate = self.get_v_rate(fly_z)
+                        rotation_rate = self.control.rotation_rate
+                        ratio = self.control.path_progress
+
+                        self.log.rotation_rate = rotation_rate
+                        self.pub_rotation_velocity.publish(rotation_rate)
+                        self.log.v_offset_rate = v_rate
+                        self.pub_v_offset_rate.publish(v_rate)
+                        self.log.ratio = ratio
+
+                        px,py = XFORM.xy_to_pxpy(fly_x,fly_y)
+                        self.src_pub.publish(px,py,fly_z)
+                        self.ack_pub.publish(True)
+
+                        #FIXME: log more stuff here for martin
+                        self.log.update()
+
+                    #150ms
+                    if (i % 60) == 0:
+                        with self.controllock:
+                            self.do_control()
+                        #FIXME: log more stuff here for martin
+
+                break
+
+            r.sleep()
+
+        rospy.loginfo('%s finished. saved data to %s' % (rospy.get_name(), self.log.close()))
+
+    def is_in_trigger_volume(self, obj):
+        c = np.array( (self.x0,self.y0) )
+        p = np.array( (obj.position.x, obj.position.y) )
+        dist = np.sqrt(np.sum((c-p)**2))
+        if (dist < START_RADIUS) and (abs(obj.position.z-START_Z) < START_ZDIST):
+            return True
+        return False
+
+    def maybe_drop_lock_on(self, now, fly_x, fly_y, fly_z):
+        dt = now - self.last_seen_time
+        if dt > TIMEOUT:
+            rospy.loginfo('TIMEOUT: time since last seen > %.1fs (%.3fs)' % (TIMEOUT,dt))
+            return True
+
+        if (fly_z > Z_MAXIMUM) or (fly_z < Z_MINIMUM):
+            return True
+
+        if np.isnan(fly_x):
+            #we have a race  - a fly to track with no pose yet
+            return False
+
+        #distance accounting, give up on fly if it is not moving
+        self.fly_dist += math.sqrt((fly_x-self.last_fly_x)**2 +
+                                   (fly_y-self.last_fly_y)**2 +
+                                   (fly_z-self.last_fly_z)**2)
+        self.last_fly_x = fly_x; self.last_fly_y = fly_y; self.last_fly_z = fly_z;
+
+        # drop slow moving flies
+        if (now - self.last_check_flying_time) > FLY_DIST_CHECK_TIME:
+            fly_dist = self.fly_dist
+            self.last_check_flying_time = now
+            self.fly_dist = 0
+            if fly_dist < FLY_DIST_MIN_DIST: # drop fly if it does not move enough
+                rospy.loginfo('SLOW: too slow (%.3f < %.3f m/s)' % (fly_dist/FLY_DIST_CHECK_TIME, FLY_DIST_MIN_DIST/FLY_DIST_CHECK_TIME))
+                return True
+
+        return False
+
+    def on_flydra_mainbrain_super_packets(self,data):
+        now = rospy.get_time()
+        flies = []
+
+        with self.trackinglock:
+            while True:
+                for packet in data.packets:
+                    for obj in packet.objects:
+                        if self.currently_locked_obj_id is None:
+                            if self.is_in_trigger_volume(obj):
+                                flies.append( MPC.Fly(obj.position.x, obj.position.y, obj.obj_id) )
+                        elif obj.obj_id == self.currently_locked_obj_id:
+                            self.last_seen_time = now
+                            self.fly = obj.position
+                            self.framenumber = packet.framenumber
+                            break
+                break
+
+            currently_locked_obj_id = self.currently_locked_obj_id
+
+        with self.controllock:
+            if currently_locked_obj_id is not None:
+                if self.maybe_drop_lock_on(now, self.fly.x, self.fly.y, self.fly.z):
+                    self.drop_lock_on()
+                    self.control.reset()
+            elif flies:
+                obj_id = self.control.should_control(flies)
+                if obj_id != -1:
+                    self.lock_on(obj_id, packet.framenumber)
+
+    def update(self):
+        self.log.update()
+        self.pub_lock_object.publish( self.log.lock_object )
+
+    def lock_on(self,obj_id,framenumber):
+        rospy.loginfo('locked object %d at frame %d' % (obj_id,framenumber))
+        now = rospy.get_time()
+        self.currently_locked_obj_id = obj_id
+        self.last_seen_time = now
+        self.first_seen_time = now
+        self.log.lock_object = obj_id
+        self.log.framenumber = framenumber
+        self.last_check_flying_time = now
+
+        self.pub_image.publish(self.img_fn)
+        self.pub_cyl_radius.publish(np.abs(self.rad_locked))
+
+        self.update()
+
+    def drop_lock_on(self):
+        old_id = self.currently_locked_obj_id
+        now = rospy.get_time()
+        dt = now - self.first_seen_time
+
+        rospy.loginfo('dropping locked object %s (tracked for %.1f)' % (old_id, dt))
+
+        self.currently_locked_obj_id = None
+
+        self.log.lock_object = IMPOSSIBLE_OBJ_ID
+        self.log.framenumber = 0
+
+        self.log.rotation_rate = 0
+        self.log.ratio = 0
+
+        self.log.cyl_r = 0.5
+
+        self.log.cyl_x = 0
+        self.log.cyl_y = 0
+
+        self.pub_image.publish(GRAY_FN)
+        self.pub_rotation_velocity.publish(0)
+        self.pub_cyl_radius.publish(0.5)
+        self.pub_cyl_centre.publish(0,0,0)
+
+        self.ack_pub.publish(False)
+
+        self.update()
+
+def main():
+    rospy.init_node("rotation")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--no-wait', action='store_true', default=False,
+                        help="dont't start unless flydra is saving data")
+    parser.add_argument('--tmpdir', action='store_true', default=False,
+                        help="store logfile in tmpdir")
+    parser.add_argument('--continue-existing', type=str, default=None,
+                        help="path to a logfile to continue")
+    argv = rospy.myargv()
+    args = parser.parse_args(argv[1:])
+
+    node = Node(
+            wait_for_flydra=not args.no_wait,
+            use_tmpdir=args.tmpdir,
+            continue_existing=args.continue_existing)
+    return node.run()
+
+if __name__=='__main__':
+    main()
+
