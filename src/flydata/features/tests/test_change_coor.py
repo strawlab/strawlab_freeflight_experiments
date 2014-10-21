8670bd7d553a1c8ddc7b08db9b3c4349fb87dc12
etienne.campione <etienne.campione@imp.ac.at>
2014-10-21 11:31:04 +0200

    add illustrated examples for the test of the coordinates changing script

--- src/flydata/features/tests/test_change_coor.py
+++ src/flydata/features/tests/test_change_coor.py
@@ -5,31 +5,31 @@ from flydata.features.coord_systems import change_coor
 
 
 def test_change_coor():
-    # Fly and stimulus in same direction, slower fly
+    # Fig. 1 (illustrated_examples.pdf): fly and stimulus in same direction, slower fly
     assert np.allclose(change_coor(3, 0, 2, 0), np.array([1, 0]))
-    # Fly and stimulus in same direction, slower stimulus
+    # Fig. 2: fly and stimulus in same direction, slower stimulus
     assert np.allclose(change_coor(2, 0, 3, 0), np.array([-1, 0]))
-    # Fly and stimulus in opposite direction
+    # Fig. 3: fly and stimulus in opposite direction
     assert np.allclose(change_coor(3, 0, -3, 0), np.array([-6, 0]))
-    # Static fly
+    # Fig. 4: static fly
     assert np.allclose(change_coor(3, 3, 0, 0), np.array([3, 3]))
-    # Static stimulus, fly aligned to the world coordinates
+    # Fig. 5: static stimulus, fly aligned to the world coordinates
     assert np.allclose(change_coor(0, 0, 3, 0), np.array([-3, 0]))
-    # Stimulus towards +x, fly towards +y
+    # Fig. 6: stimulus towards +x, fly towards +y
     assert np.allclose(change_coor(3, 0, 0, 3), np.array([-3, -3]))
-    # Stimulus towards +x, fly towards -y
+    # Fig. 7: stimulus towards +x, fly towards -y
     assert np.allclose(change_coor(3, 0, 0, -3), np.array([-3, 3]))
-    # Stimulus towards -x, fly towars -y
+    # Fig. 8: stimulus towards -x, fly towars -y
     assert np.allclose(change_coor(-3, 0, 0, -3), np.array([-3, -3]))
-    # Static stimulus, fly at 45 deg
+    # Fig. 9: static stimulus, fly at 45 deg
     assert np.allclose(change_coor(0, 0, 3, 3), np.array([-np.sqrt(18), 0]))
-    # Fly and stimulus in opposite direction, diagonally with regard to the world's coordinates
+    # Fig. 10: fly and stimulus in opposite direction, diagonally with regard to the world's coordinates
     assert np.allclose(change_coor(-3, -3, 3, 3), np.array([-2*np.sqrt(18), 0]))
-    # Fly like previously, stimulus towards +y
+    # Fig. 11: fly like previously, stimulus towards +y
     assert np.allclose(change_coor(0, 3, 3, 3), np.array([-(3/2.)*np.sqrt(2), (3/2.)*np.sqrt(2)]))
-    # Fly at 225 deg, stimulus towards +y
+    # Fig. 12: fly at 225 deg, stimulus towards +y
     assert np.allclose(change_coor(0, 3, -3, -3), np.array([-np.sqrt(18)-(3/2.)*np.sqrt(2), -(3/2.)*np.sqrt(2)]))
-    # Vectorized
+    # Vectorized (fig. 1, 10 and 12)
     stim_vel_x = np.array([3, -3, 0])
     stim_vel_y = np.array([0, -3, 3])
     fly_vel_x = np.array([2, 3, -3])
