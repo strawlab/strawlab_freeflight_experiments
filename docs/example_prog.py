d2a1975aa16571986ea24687ea9d3d88cce89c36
santi <sdvillal@gmail.com>
2014-11-28 14:28:18 +0100

    adapts example to combine not storing trial objects anymore

--- docs/example_prog.py
+++ docs/example_prog.py
@@ -14,8 +14,3 @@ df,dt,(x0,y0,obj_id,framenumber0,time0) = combine.get_one_result(4984)
 
 #get all results (se the docs for the structure of the results dictionary)
 results, dt = combine.get_results()
-
-#analyse the data the new way (Santi's API)
-for exp in combine.get_trajs():
-    print exp.uuid(), exp.md().description()
-    break #only print one
