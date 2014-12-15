b9dd84563f9326da325be865b1bc4d937edaa5d8
John Stowers <john.stowers@gmail.com>
2014-12-15 15:07:48 +0100

    nodelib.log: support for new condition object

--- src/nodelib/log.py
+++ src/nodelib/log.py
@@ -6,8 +6,10 @@ import tempfile
 import threading
 
 import roslib; roslib.load_manifest('strawlab_freeflight_experiments')
+
 import rospy
 import std_msgs.msg
+import strawlab_freeflight_experiments.conditions
 
 class NoDataError(Exception):
     pass
@@ -110,7 +112,10 @@ class CsvLogger(object):
             rospy.Subscriber('experiment_uuid',
                              std_msgs.msg.String,
                              self._on_experiment_uuid)
-            self._pub_condition = rospy.Publisher('condition', std_msgs.msg.String)
+
+            self._pub_condition_s = rospy.Publisher('condition_slash', std_msgs.msg.String)
+            self._pub_condition_y = rospy.Publisher('condition_yaml', std_msgs.msg.String)
+            self._pub_condition_n = rospy.Publisher('condition_name', std_msgs.msg.String)
 
             if wait:
                 self._debug("waiting for flydra_mainbrain/data_file")
@@ -152,15 +157,28 @@ class CsvLogger(object):
 
     @condition.setter
     def condition(self, v):
-        self._condition = v
-        if self._pub_condition is not None:
-            try:
-                self._pub_condition.publish(v)
-            except rospy.ROSException:
-                #node not yet initialized, which is usually just in test cases,
-                #in any case, this is not fatal even in practice and publishing
-                #condition is only for the operator-console anyway
-                pass
+        if v is None:
+            return
+
+        if not isinstance(v, strawlab_freeflight_experiments.conditions.Condition):
+            raise ValueError("YOU MUST ASSIGN CONDITION OBJECT TO LOG")
+
+        s = v.to_slash_separated()
+        y = v.to_yaml()
+        n = v.name
+
+        #still write the slash separated to the csv
+        self._condition = s
+
+        for val,pub in zip((s,y,n),(self._pub_condition_s,self._pub_condition_y,self._pub_condition_n)):
+            if pub is not None:
+                try:
+                    pub.publish(val)
+                except rospy.ROSException:
+                    #node not yet initialized, which is usually just in test cases,
+                    #in any case, this is not fatal even in practice and publishing
+                    #condition is only for the operator-console anyway
+                    pass
 
     @property
     def filename(self):
