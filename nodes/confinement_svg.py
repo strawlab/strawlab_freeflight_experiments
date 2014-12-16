266916574383034c74d8624becb6899c72c891fb
John Stowers <john.stowers@gmail.com>
2014-12-16 18:31:10 +0100

    confinement: unify with other nodes

--- nodes/confinement_svg.py
+++ nodes/confinement_svg.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python
 
-import os
-import numpy as np
-import time
-import argparse
-import threading
 import math
+import numpy as np
+import threading
+import argparse
+import os.path
 
 PACKAGE='strawlab_freeflight_experiments'
+
 import roslib
+import roslib.packages
 roslib.load_manifest(PACKAGE)
+
 import rospy
 import flyvr.display_client as display_client
-from std_msgs.msg import String, UInt32, Bool, Float32
-from geometry_msgs.msg import Vector3, Pose, Polygon, Point
+from std_msgs.msg import UInt32, Bool, Float32, String
+from geometry_msgs.msg import Vector3, Pose, Polygon
 from ros_flydra.msg import flydra_mainbrain_super_packet
 
 import flyflypath.transform
 import nodelib.log
 import nodelib.visualization
 
-from ros_flydra.constants import IMPOSSIBLE_OBJ_ID, IMPOSSIBLE_OBJ_ID_ZERO_POSE
+from ros_flydra.constants import IMPOSSIBLE_OBJ_ID
 
 pkg_dir = roslib.packages.get_pkg_dir(PACKAGE)
+
+CONTROL_RATE        = 40.0      #Hz
+SWITCH_MODE_TIME    = 5.0*60    #alternate between control and static (i.e. experimental control) seconds
 
 HOLD_COND = "midgray.osg"
 
@@ -42,18 +47,19 @@ CONDITIONS = [
 START_CONDITION = 1
 COOL_CONDITIONS = set(["lboxmed.svg.osg/+0.0/+0.0/0"])#set()
 
-CONTROL_RATE = 40.0
 
-SWITCH_MODE_TIME    = 5.0*60
 FLY_DIST_CHECK_TIME = 5.0
-
-FLY_DIST_MIN_DIST = 0.15
+FLY_DIST_MIN_DIST   = 0.2
 
 START_RADIUS    = 0.12
 START_ZDIST     = 0.4
 START_Z         = 0.5
 
-TIMEOUT = 0.5
+# z range for fly tracking (dropped outside)
+Z_MINIMUM = 0.00
+Z_MAXIMUM = 0.95
+
+TIMEOUT             = 0.5
 
 XFORM = flyflypath.transform.SVGTransform()
 
@@ -66,28 +72,27 @@ class Node(object):
         self._pub_stim_mode = display_client.DisplayServerProxy.set_stimulus_mode(
             'StimulusOSGFile')
 
-        self.log = Logger(wait=wait_for_flydra, use_tmpdir=use_tmpdir, continue_existing=continue_existing)
-
         self.pub_stimulus = rospy.Publisher('stimulus_filename', String, latch=True, tcp_nodelay=True)
-        self.pub_lock_object = rospy.Publisher('lock_object', UInt32, latch=True, tcp_nodelay=True)
         self.pub_lag = rospy.Publisher('extra_lag_msec', Float32, latch=True, tcp_nodelay=True)
         self.pub_model_pose = rospy.Publisher('model_pose', Pose, latch=True, tcp_nodelay=True)
 
-        #publish for the follow_path monitor
-        self.svg_pub = rospy.Publisher('svg_filename', String, latch=True, tcp_nodelay=True)
-        self.srcpx_pub = rospy.Publisher('source', Vector3, latch=False, tcp_nodelay=True)
-        self.active_pub = rospy.Publisher('active', Bool, latch=True, tcp_nodelay=True)
         self.trigarea_pub = rospy.Publisher('trigger_area', Polygon, latch=True, tcp_nodelay=True)
 
         self.pushover_pub = rospy.Publisher('note', String)
         self.save_pub = rospy.Publisher('save_object', UInt32)
 
-        #protect the traked id and fly position between the time syncronous main loop and the asyn
+        self.pub_lock_object = rospy.Publisher('lock_object', UInt32, latch=True, tcp_nodelay=True)
+        self.pub_lock_object.publish(IMPOSSIBLE_OBJ_ID)
+
+        self.log = Logger(wait=wait_for_flydra, use_tmpdir=use_tmpdir, continue_existing=continue_existing)
+
+        #protect the tracked id and fly position between the time syncronous main loop and the asyn
         #tracking/lockon/off updates
         self.trackinglock = threading.Lock()
         with self.trackinglock:
             self.currently_locked_obj_id = None
             self.fly = Vector3()
+            self.flyv = Vector3()
             self.framenumber = 0
             now = rospy.get_time()
             self.first_seen_time = now
@@ -96,17 +101,21 @@ class Node(object):
             self.last_check_flying_time = now
             self.fly_dist = 0
 
-        self.timer = rospy.Timer(rospy.Duration(SWITCH_MODE_TIME), # switch every 5 minutes
-                                  self.switch_conditions)
-        self.switch_conditions(None,force=START_CONDITION)
+        self.svg_pub = rospy.Publisher("svg_filename", String, latch=True)
+        self.src_pub = rospy.Publisher("source", Vector3)
+        self.ack_pub = rospy.Publisher("active", Bool)
 
-        self.srcpx_pub.publish(0,0,0)
-        self.active_pub.publish(False)
+		self.x0 = self.y0 = 0
         self.trigarea_pub.publish(
                 nodelib.visualization.get_circle_trigger_volume_polygon(
                                         XFORM,
                                         START_RADIUS,self.x0,self.y0)
         )
+
+        self.switch_conditions(None,force=START_CONDITION)
+
+        self.timer = rospy.Timer(rospy.Duration(SWITCH_MODE_TIME),
+                                  self.switch_conditions)
 
         rospy.Subscriber("flydra_mainbrain/super_packets",
                          flydra_mainbrain_super_packet,
@@ -152,6 +161,7 @@ class Node(object):
             with self.trackinglock:
                 currently_locked_obj_id = self.currently_locked_obj_id
                 fly_x = self.fly.x; fly_y = self.fly.y; fly_z = self.fly.z
+                fly_vx = self.flyv.x; fly_vy = self.flyv.y; fly_vz = self.flyv.z
                 framenumber = self.framenumber
 
             if currently_locked_obj_id is None:
@@ -160,9 +170,10 @@ class Node(object):
                 now = rospy.get_time()
                 if now-self.last_seen_time > TIMEOUT:
                     self.drop_lock_on()
+                    rospy.loginfo('TIMEOUT: time since last seen >%.1fs' % (TIMEOUT))
                     continue
 
-                if (fly_z > 0.95) or (fly_z < 0):
+                if (fly_z > Z_MAXIMUM) or (fly_z < Z_MINIMUM):
                     self.drop_lock_on()
                     continue
 
@@ -177,23 +188,26 @@ class Node(object):
                                            (fly_y-self.last_fly_y)**2 +
                                            (fly_z-self.last_fly_z)**2)
                 self.last_fly_x = fly_x; self.last_fly_y = fly_y; self.last_fly_z = fly_z;
+
+                # drop slow moving flies
                 if now-self.last_check_flying_time > FLY_DIST_CHECK_TIME:
                     fly_dist = self.fly_dist
                     self.last_check_flying_time = now
                     self.fly_dist = 0
-                    if fly_dist < FLY_DIST_MIN_DIST:
+                    if fly_dist < FLY_DIST_MIN_DIST: # drop fly if it does not move enough
                         self.drop_lock_on()
+                        rospy.loginfo('SLOW: too slow (%.3f < %.3f m/s)' % (fly_dist/FLY_DIST_CHECK_TIME, FLY_DIST_MIN_DIST/FLY_DIST_CHECK_TIME))
                         continue
 
                 px,py = XFORM.xy_to_pxpy(fly_x,fly_y)
-                self.srcpx_pub.publish(px,py,fly_z)
+                self.src_pub.publish(px,py,fly_z)
 
             #don't need to record anything at the control rate
             #self.log.framenumber = framenumber
             #self.log.update()
 
 
-            self.active_pub.publish(active)
+            self.ack_pub.publish(active)
 
             r.sleep()
 
@@ -233,6 +247,7 @@ class Node(object):
             self.first_seen_time = now
             self.log.lock_object = obj.obj_id
             self.log.framenumber = framenumber
+            self.last_check_flying_time = now
 
         self.pub_stimulus.publish( self.stimulus_filename )
         self.update()
