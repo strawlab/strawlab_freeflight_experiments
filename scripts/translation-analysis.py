f66cd5394ed75b56e28551bfec70fb83f8fc31a3
santi <sdvillal@gmail.com>
2015-01-08 16:37:51 +0100

    plots: filtering and plotting by experiment running time poc

--- scripts/translation-analysis.py
+++ scripts/translation-analysis.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python2
 import os.path
 import sys
-import operator
-import numpy as np
-import itertools
+from datetime import datetime, timedelta
+
+from autodata.model import find_experiment
+
 
 if not os.environ.get('DISPLAY'):
     print "DISPLAY NOT SET: USING AGG BACKEND"
@@ -21,10 +22,36 @@ import analysislib.plots as aplt
 import analysislib.curvature as curve
 import analysislib.util as autil
 
+
+def filter_trials_after_start(combine,
+                              delta=timedelta(hours=2)):
+    """
+    :type combine: analysislib.combine._Combine
+    """
+    # retrieve the experiment start times
+    datetime_limits = {}
+
+    def keep_before(unused1, soid, uuid, unused2):
+        if uuid is None:
+            return False  # Can happen even in basic examples, probably because of race conditions
+        if uuid not in datetime_limits:
+            exp_md = find_experiment(uuid)[2]
+            exp_start = exp_md['start_secs'] + 1E-9 * exp_md['start_nsecs']
+            datetime_limits[uuid] = datetime.fromtimestamp(exp_start) + delta
+        time0 = datetime.fromtimestamp(soid[-1])  # time0 is the last field
+        return time0 < datetime_limits[uuid]
+
+    return combine.filter_trials(keep_before)
+
+
 if __name__=='__main__':
     parser = analysislib.args.get_parser()
 
     args = parser.parse_args()
+
+    # hack: separate by uuid even if we specify an outdir
+    if args.outdir is not None and not args.outdir.endswith(args.uuid[0]):
+        args.outdir = os.path.join(args.outdir, args.uuid[0])
 
     analysislib.args.check_args(parser, args)
 
@@ -54,6 +81,26 @@ if __name__=='__main__':
     aplt.plot_traces(combine, args,
                 figncols=ncond,
                 in3d=True)
+
+    # filter out trajectories that start after some time; plot
+    # this is not something we want to do all the time so we can:
+    #   - nasty comment-uncomment
+    #   - add a new filter to the command line of combine scripts
+    #   - create a script that will just do this
+    #   - ...
+    combine_before = filter_trials_after_start(combine, delta=timedelta(hours=1))
+
+    aplt.plot_traces(combine_before, args,
+                     name='combine-before',  # FIXME: name does not respect per-script directory, writes in root
+                     figncols=ncond,
+                     in3d=False,
+                     show_starts=True,
+                     show_ends=True)
+
+    aplt.plot_traces(combine_before, args,
+                     name='combine-before-3D',
+                     figncols=ncond,
+                     in3d=True)
 
     aplt.plot_histograms(combine, args,
                 figncols=ncond)
