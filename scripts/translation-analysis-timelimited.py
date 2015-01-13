48b5b071287adf648826d25c9c99e638f48635e9
etienne.campione <etienne.campione@imp.ac.at>
2015-01-13 14:09:26 +0100

    separate the translation analysis script in two scripts: analysis during the whole experiment + analysis during 6 first hours

--- scripts/translation-analysis-timelimited.py
+++ scripts/translation-analysis-timelimited.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 import os.path
 import sys
+from datetime import datetime, timedelta
 
 if not os.environ.get('DISPLAY'):
     print "DISPLAY NOT SET: USING AGG BACKEND"
@@ -10,6 +11,7 @@ if not os.environ.get('DISPLAY'):
 import roslib
 roslib.load_manifest('strawlab_freeflight_experiments')
 
+from autodata.model import find_experiment
 import autodata.files
 import analysislib.filters
 import analysislib.combine
@@ -18,10 +20,36 @@ import analysislib.plots as aplt
 import analysislib.curvature as curve
 import analysislib.util as autil
 
+
+def filter_trials_after_start(combine,
+                              delta=timedelta(hours=2)):
+    """
+    :type combine: analysislib.combine._Combine
+    """
+    # retrieve the experiment start times
+    datetime_limits = {}
+
+    def keep_before(unused1, soid, uuid, unused2):
+        if uuid is None:
+            return False  # Can happen even in basic examples, probably because of race conditions
+        if uuid not in datetime_limits:
+            exp_md = find_experiment(uuid)[2]
+            exp_start = exp_md['start_secs'] + 1E-9 * exp_md['start_nsecs']
+            datetime_limits[uuid] = datetime.fromtimestamp(exp_start) + delta
+        time0 = datetime.fromtimestamp(soid[-1])  # time0 is the last field
+        return time0 < datetime_limits[uuid]
+
+    return combine.filter_trials(keep_before)
+
+
 if __name__=='__main__':
     parser = analysislib.args.get_parser()
 
     args = parser.parse_args()
+
+    # hack: separate by uuid even if we specify an outdir
+    if args.outdir is not None and not args.outdir.endswith(args.uuid[0]):
+        args.outdir = os.path.join(args.outdir, args.uuid[0])
 
     analysislib.args.check_args(parser, args)
 
@@ -35,27 +63,33 @@ if __name__=='__main__':
     print "files saved as", fname
     ncond = combine.get_num_conditions()
 
-    aplt.save_args(combine, args)
-    aplt.save_results(combine, args)
+    # filter out trajectories that start after some time; plot
+    # this is not something we want to do all the time so we can:
+    #   - nasty comment-uncomment
+    #   - add a new filter to the command line of combine scripts
+    #   - create a script that will just do this
+    #   - ...
+    combine_before = filter_trials_after_start(combine, delta=timedelta(hours=6)) #change delta here
 
-    aplt.save_most_loops(combine, args)
+    aplt.save_args(combine_before, args)
+    aplt.save_results(combine_before, args)
 
-    aplt.plot_trial_times(combine, args)
+    aplt.save_most_loops(combine_before, args)
 
-    aplt.plot_traces(combine, args,
-                figncols=ncond,
-                in3d=False,
-                show_starts=True,
-                show_ends=True)
+    aplt.plot_traces(combine_before, args,
+                     name=fname + '.6h.traces',
+                     figncols=ncond,
+                     in3d=False,
+                     show_starts=True,
+                     show_ends=True)
 
-    aplt.plot_traces(combine, args,
-                figncols=ncond,
-                in3d=True)
+    aplt.plot_traces(combine_before, args,
+                     name=fname + '.6h.traces.3d',
+                     figncols=ncond,
+                     in3d=True)
 
-    aplt.plot_histograms(combine, args,
+    aplt.plot_histograms(combine_before, args,
                 figncols=ncond)
-
-    aplt.plot_nsamples(combine, args)
 
     if args.plot_tracking_stats and len(args.uuid) == 1:
         fplt = autodata.files.FileView(
@@ -85,11 +119,6 @@ if __name__=='__main__':
                          "xlabel":{"velocity":"velocity (m/s)",
                                    "dtheta":"turn rate (rad/s)"},
     }
-
-    flat_data,nens = curve.flatten_data(args, combine, histograms)
-    curve.plot_histograms(args, combine, flat_data, nens, histograms, histogram_options)
-
-    curve.plot_correlation_analysis(args, combine, correlations, correlation_options)
 
     if args.show:
         aplt.show_plots()
