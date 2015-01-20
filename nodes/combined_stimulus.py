c3a929a5aa2e3e2c5033f39dee80a760881febe2
John Stowers <john.stowers@gmail.com>
2015-01-20 14:27:59 +0100

    combined_stimulus: move condition switching to base class

--- nodes/combined_stimulus.py
+++ nodes/combined_stimulus.py
@@ -31,7 +31,6 @@ from strawlab_freeflight_experiments import INVALID_VALUE
 pkg_dir = roslib.packages.get_pkg_dir(PACKAGE)
 
 CONTROL_RATE        = 80.0      #Hz
-SWITCH_MODE_TIME    = 5.0*60    #alternate between control and static (i.e. experimental control) seconds
 
 ADVANCE_RATIO       = 1/100.0
 
@@ -133,10 +132,7 @@ class Node(nodelib.node.Experiment):
         self.trg_pub = rospy.Publisher("target", Vector3)
         self.ack_pub = rospy.Publisher("active", Bool)
 
-        self.switch_conditions(force=True)
-
-        self.timer = rospy.Timer(rospy.Duration(SWITCH_MODE_TIME),
-                                  self.switch_conditions)
+        self.switch_conditions()
 
         rospy.Subscriber("flydra_mainbrain/super_packets",
                          flydra_mainbrain_super_packet,
@@ -153,13 +149,7 @@ class Node(nodelib.node.Experiment):
     def is_replay_experiment_z(self):
         return np.isnan(self.v_gain)
 
-    def switch_conditions(self,event=None,force=False):
-        if force:
-            self.condition = self.conditions[self.start_condition]
-        else:
-            self.condition = self.conditions.next_condition(self.condition)
-
-        self.log.condition = self.condition
+    def switch_conditions(self):
 
         self.drop_lock_on()
 
