f40e79565964a81847ca40717d7e3a9acc18776a
santi <sdvillal@gmail.com>
2015-01-28 12:10:27 +0100

    conditions: tests for condition switching strategies

--- test/test_conditions.py
+++ test/test_conditions.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os.path
+from time import sleep
 import unittest
 
 import roslib
@@ -55,6 +56,59 @@ rotation_infinity:
         self.assertEqual(afixes.normalize_condition_string("-3.0"),"-3.0")
         self.assertEqual(afixes.normalize_condition_string("+3.0"),"3.0")
         self.assertEqual(afixes.normalize_condition_string("+3.00"),"3.0")
+
+    def test_randomisation(self):
+        s = """
+cond1:
+    gain: 0.3
+cond2:
+    gain: 0.5
+cond3:
+    gain: 0.7
+"""
+        Conditions = sfe_conditions.Conditions
+
+        def next_conditions(conds, n=9999):
+            conditions = [conds.first_condition()]
+            while len(conditions) < n:
+                conditions.append(conds.next_condition(conditions[-1]))
+            return [cond.name for cond in conditions]
+
+        # sequential
+        cond_names = next_conditions(Conditions(s, switch_order='seq'))
+        self.assertEquals(['cond1', 'cond2', 'cond3'] * 3333, cond_names)
+
+        # random is random, but controlled
+        cond_names1 = next_conditions(Conditions(s, switch_order='fullrand', rng_seed=42))
+        cond_names2 = next_conditions(Conditions(s, switch_order='fullrand', rng_seed=42))
+        cond_names3 = next_conditions(Conditions(s, switch_order='fullrand', rng_seed=2147483647))
+        self.assertEquals(cond_names1, cond_names2)
+        self.assertNotEquals(cond_names1, cond_names3)
+
+        # default is controlled (remove if we change the policy)
+        cond_names1 = next_conditions(Conditions(s, switch_order='fullrand'))
+        sleep(0.001)  # overkill
+        cond_names2 = next_conditions(Conditions(s, switch_order='fullrand'))
+        self.assertEquals(cond_names1, cond_names2)
+
+        # proportions - serves as example in case we want to test non-uniform samplers
+        self.assertAlmostEqual(cond_names1.count('cond1') / float(len(cond_names1)), 1/3., delta=0.01)
+        self.assertAlmostEqual(cond_names1.count('cond2') / float(len(cond_names1)), 1/3., delta=0.01)
+        self.assertAlmostEqual(cond_names1.count('cond3') / float(len(cond_names1)), 1/3., delta=0.01)
+
+        # clock-based seed
+        cond_names1 = next_conditions(Conditions(s, switch_order='fullrand', rng_seed=-1))
+        sleep(0.001)  # overkill
+        cond_names2 = next_conditions(Conditions(s, switch_order='fullrand', rng_seed=-1))
+        self.assertNotEquals(cond_names1, cond_names2)
+
+        # randstart
+        cond_names1 = next_conditions(Conditions(s, switch_order='randstart', rng_seed=42))
+        cond_names2 = next_conditions(Conditions(s, switch_order='randstart', rng_seed=42))
+        self.assertNotEquals(['cond1', 'cond2', 'cond3'], cond_names1[:3])  # by chance, true, change seed if not
+        self.assertEquals(cond_names1, cond_names2)
+        self.assertEquals(cond_names1[0:3] * 3333, cond_names1)
+
 
 if __name__ == '__main__':
     unittest.main()
