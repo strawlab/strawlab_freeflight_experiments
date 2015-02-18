combine: fixes tns computation

src/analysislib/combine.py
@@ -1493,9 +1493,10 @@ class CombineH5WithCSV(_Combine):
                 start_x = valid['x'][0]
                 start_y = valid['y'][0]
 
-                # provide a nanoseconds after the epoc column
+                # provide a nanoseconds after the epoc column (use at your own risk(TM))
                 if 'tns' not in df.columns:
-                    df['tns'] = len(df) * dt + start_time
+                    df['tns'] = np.arange(len(df)) * dt + start_time
+                    # should be close too to: df['t_secs'] + 1E-9 * df[t_nsecs]
 
                 r['count'] += 1
                 r['start_obj_ids'].append((start_x, start_y, oid, start_framenumber, start_time))