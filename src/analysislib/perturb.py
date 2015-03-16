0ba2adfaa03789c5edf95e15e368d0c989a51cfd
santi <sdvillal@gmail.com>
2015-03-16 12:16:09 +0100

    perturb: collect_traces returns a list (+pep8)

--- src/analysislib/perturb.py
+++ src/analysislib/perturb.py
@@ -1,20 +1,29 @@
-import time
 import collections
+
 import numpy as np
-import pandas as pd
 
 import roslib
+
 roslib.load_manifest('strawlab_freeflight_experiments')
 
 import strawlab_freeflight_experiments.perturb as sfe_perturb
 
-PerturbationHolder = collections.namedtuple('PerturbationHolder', 'df start_idx end_idx obj_id completed completed_pct start_ratio, perturbation_length, trajectory_length, condition')
+PerturbationHolder = collections.namedtuple('PerturbationHolder',
+                                            'uuid, obj_id, start_frame, '
+                                            'condition, '
+                                            'start_ratio, '
+                                            'start_idx, end_idx, '
+                                            'completed, completed_pct, '
+                                            'perturbation_length, trajectory_length, '
+                                            'df')
+
 
 def collect_perturbation_traces(combine, completion_threshold=0.98):
-    results,dt = combine.get_results()
 
-    perturbations = collections.OrderedDict()           #cond: {obj_id:PerturbationHolder,...}
-    perturbation_objects = {}                           #cond: perturb_obj
+    results, dt = combine.get_results()
+
+    perturbations = []          # list of PerturbationHolder objects
+    perturbation_objects = {}   # cond: perturb_obj
 
     for cond in sorted(results):
 
@@ -23,16 +32,16 @@ def collect_perturbation_traces(combine, completion_threshold=0.98):
             try:
                 step_obj = sfe_perturb.get_perturb_object_from_condition(condition_conf)
             except KeyError:
-                #new style yaml experiment, not a perturbation condition
+                # new style yaml experiment, not a perturbation condition
                 continue
         else:
-            #backwards compatibility for old pre-yaml experiments where the perturb_descripor
-            #was assumed to be the last element in the condition string
+            # backwards compatibility for old pre-yaml experiments where the perturb_descripor
+            # was assumed to be the last element in the condition string
             perturb_desc = cond.split("/")[-1]
             step_obj = sfe_perturb.get_perturb_object(perturb_desc)
 
-        #only plot perturbations
-        if isinstance(step_obj,sfe_perturb.NoPerturb):
+        # only plot perturbations
+        if isinstance(step_obj, sfe_perturb.NoPerturb):
             continue
 
         perturbations[cond] = {}
@@ -40,28 +49,26 @@ def collect_perturbation_traces(combine, completion_threshold=0.98):
 
         r = results[cond]
 
-        for _df,(x0,y0,obj_id,framenumber0,time0) in zip(r['df'], r['start_obj_ids']):
+        # Work even if no uuids are present
+        uuids = r['uuids'] if 'uuids' in r else []
+        if len(uuids) != len(r['df']):
+            uuids = [None] * len(r['df'])
 
-            df = _df.fillna(method='ffill')
+        for df, (x0, y0, obj_id, framenumber0, time0), uuid in zip(r['df'], r['start_obj_ids'], uuids):
+
+            df = df.fillna(method='ffill')
             df['condition'] = cond
 
-            #find the start of the perturbation (where perturb_progress == 0)
+            # find the start of the perturbation (where perturb_progress == 0)
             z = np.where(df['perturb_progress'].values == 0)
             if len(z[0]):
                 fidx = z[0][0]
 
-                #find the index of the last perturbation (max -1)
+                # find the index of the last perturbation (max -1)
                 l = np.where(df['perturb_progress'].values == df['perturb_progress'].max())
                 lidx = l[0][0]
 
-                #ensure we get a unique obj_id for later grouping. That is not necessarily
-                #guarenteed because obj_ids may be in multiple conditions, so if need be
-                #create a new one
-                if obj_id in perturbations[cond]:
-                    obj_id = int(time.time()*1e6)
-                df['obj_id'] = obj_id
-
-                t = time0 + (np.arange(0,len(df),dtype=float) * dt)
+                t = time0 + (np.arange(0, len(df), dtype=float) * dt)
                 df['time'] = t
                 df['talign'] = t - t[fidx]
 
@@ -73,21 +80,28 @@ def collect_perturbation_traces(combine, completion_threshold=0.98):
 
                 df['align'] = np.array(range(len(df)), dtype=int) - fidx
 
-                #save both the exact value, and an identifier to signify which
-                #part of the arena the perturbation started in. The identifier
-                #is based on the ratio range_funcs, range_chunks magic. A range string
-                #0.4|0.46|0.5|0.8 is the pairs of ranges (0.4->0.46) and (0.5->0.8).
-                #if the fly started its perturbation with a ratio of 0.55 the range
-                #chunk identifier is 1 (the 2nd pair in the range string).
+                # save both the exact value, and an identifier to signify which
+                # part of the arena the perturbation started in. The identifier
+                # is based on the ratio range_funcs, range_chunks magic. A range string
+                # 0.4|0.46|0.5|0.8 is the pairs of ranges (0.4->0.46) and (0.5->0.8).
+                # if the fly started its perturbation with a ratio of 0.55 the range
+                # chunk identifier is 1 (the 2nd pair in the range string).
                 start_ratio = df.iloc[fidx]['ratio']
                 start_id = step_obj.get_perturb_range_identifier(start_ratio)
 
                 df['ratio_range_start_id'] = start_id
 
-                ph_obj = PerturbationHolder(df, fidx, lidx, obj_id, completed, completed_pct, start_ratio, tmax, traj_length, cond)
+                ph_obj = PerturbationHolder(uuid=uuid, obj_id=obj_id, start_frame=framenumber0,
+                                            condition=cond,
+                                            start_ratio=start_ratio,
+                                            start_idx=fidx, end_idx=lidx,
+                                            completed=completed, completed_pct=completed_pct,
+                                            perturbation_length=tmax, trajectory_length=traj_length,
+                                            df=df)
                 perturbations[cond][obj_id] = ph_obj
 
     return perturbations, perturbation_objects
+
 
 def get_input_output_columns(step_obj):
     if step_obj.what == 'rotation_rate':
@@ -100,7 +114,3 @@ def get_input_output_columns(step_obj):
         raise Exception("Not supported: %s" % step_obj.what)
 
     return system_u, system_y
-
-
-
-
