144d3d195776f07672101d3b0c93c731667beb18
santi <sdvillal@gmail.com>
2015-03-17 03:33:47 +0100

    perturb: extract method

--- src/analysislib/perturb.py
+++ src/analysislib/perturb.py
@@ -37,6 +37,58 @@ def find_step_obj(cond, condition_conf=None):
     return None if isinstance(step_obj, sfe_perturb.NoPerturb) else step_obj
 
 
+def extract_perturbations(df, uuid, obj_id, framenumber0, cond, time0, dt,
+                          step_obj, completion_threshold=0.98):
+    """Returns a list with the perturbations for a single trial."""
+
+    df = df.fillna(method='ffill')
+    df['condition'] = cond
+
+    # find the start of the perturbation (where perturb_progress == 0)
+    z = np.where(df['perturb_progress'].values == 0)
+    if len(z[0]):
+        fidx = z[0][0]
+
+        # find the index of the last perturbation (max -1)
+        l = np.where(df['perturb_progress'].values == df['perturb_progress'].max())
+        lidx = l[0][0]
+
+        t = time0 + (np.arange(0, len(df), dtype=float) * dt)
+        df['time'] = t
+        df['talign'] = t - t[fidx]
+
+        tmax = df['talign'].max()
+        traj_length = tmax - df['talign'].min()
+
+        completed = step_obj.completed_perturbation(tmax, completion_threshold) and (lidx > fidx)
+        completed_pct = step_obj.completed_perturbation_pct(tmax, completion_threshold)
+
+        df['align'] = np.array(range(len(df)), dtype=int) - fidx
+
+        # save both the exact value, and an identifier to signify which
+        # part of the arena the perturbation started in. The identifier
+        # is based on the ratio range_funcs, range_chunks magic. A range string
+        # 0.4|0.46|0.5|0.8 is the pairs of ranges (0.4->0.46) and (0.5->0.8).
+        # if the fly started its perturbation with a ratio of 0.55 the range
+        # chunk identifier is 1 (the 2nd pair in the range string).
+        start_ratio = df.iloc[fidx]['ratio']
+        start_id = step_obj.get_perturb_range_identifier(start_ratio)
+
+        df['ratio_range_start_id'] = start_id
+
+        ph_obj = PerturbationHolder(uuid=uuid, obj_id=obj_id, start_frame=framenumber0,
+                                    perturb_id=0,
+                                    condition=cond,
+                                    start_ratio=start_ratio,
+                                    start_idx=fidx, end_idx=lidx,
+                                    completed=completed, completed_pct=completed_pct,
+                                    perturbation_length=tmax, trajectory_length=traj_length,
+                                    df=df)
+        return [ph_obj]
+
+    return []
+
+
 def collect_perturbation_traces(combine, completion_threshold=0.98):
 
     results, dt = combine.get_results()
@@ -62,52 +114,15 @@ def collect_perturbation_traces(combine, completion_threshold=0.98):
             uuids = [None] * len(r['df'])
 
         for df, (x0, y0, obj_id, framenumber0, time0), uuid in zip(r['df'], r['start_obj_ids'], uuids):
-
-            df = df.fillna(method='ffill')
-            df['condition'] = cond
-
-            # find the start of the perturbation (where perturb_progress == 0)
-            z = np.where(df['perturb_progress'].values == 0)
-            if len(z[0]):
-                fidx = z[0][0]
-
-                # find the index of the last perturbation (max -1)
-                l = np.where(df['perturb_progress'].values == df['perturb_progress'].max())
-                lidx = l[0][0]
-
-                t = time0 + (np.arange(0, len(df), dtype=float) * dt)
-                df['time'] = t
-                df['talign'] = t - t[fidx]
-
-                tmax = df['talign'].max()
-                traj_length = tmax - df['talign'].min()
-
-                completed = step_obj.completed_perturbation(tmax, completion_threshold) and (lidx > fidx)
-                completed_pct = step_obj.completed_perturbation_pct(tmax, completion_threshold)
-
-                df['align'] = np.array(range(len(df)), dtype=int) - fidx
-
-                # save both the exact value, and an identifier to signify which
-                # part of the arena the perturbation started in. The identifier
-                # is based on the ratio range_funcs, range_chunks magic. A range string
-                # 0.4|0.46|0.5|0.8 is the pairs of ranges (0.4->0.46) and (0.5->0.8).
-                # if the fly started its perturbation with a ratio of 0.55 the range
-                # chunk identifier is 1 (the 2nd pair in the range string).
-                start_ratio = df.iloc[fidx]['ratio']
-                start_id = step_obj.get_perturb_range_identifier(start_ratio)
-
-                df['ratio_range_start_id'] = start_id
-
-                ph_obj = PerturbationHolder(uuid=uuid, obj_id=obj_id, start_frame=framenumber0,
-                                            perturb_id=0,
-                                            condition=cond,
-                                            start_ratio=start_ratio,
-                                            start_idx=fidx, end_idx=lidx,
-                                            completed=completed, completed_pct=completed_pct,
-                                            perturbation_length=tmax, trajectory_length=traj_length,
-                                            df=df)
-
-                perturbations.append(ph_obj)
+            perturbations.extend(extract_perturbations(df,
+                                                       uuid,
+                                                       obj_id,
+                                                       framenumber0,
+                                                       cond,
+                                                       time0,
+                                                       dt,
+                                                       step_obj,
+                                                       completion_threshold))
 
     return perturbations, perturbation_objects
 
