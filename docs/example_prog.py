3ae4984b793e041891fd10f13c1a1bf2831773e0
John Stowers <john.stowers@gmail.com>
2015-05-12 11:04:01 +0200

    example: show how to add a feature

--- docs/example_prog.py
+++ docs/example_prog.py
@@ -2,15 +2,14 @@ import roslib
 roslib.load_manifest('strawlab_freeflight_experiments')
 import analysislib.util
 
-UUID = "d21b4ff80c0611e3bfcc6c626d3a008a"
+UUID = "17b2a814ee8b11e4b9316c626d3a008a"
 
 combine = analysislib.util.get_combiner_for_uuid(UUID)
-combine.disable_debug()
+combine.add_feature(column_name='saccade')
 combine.add_from_uuid(UUID)
 
-#### analyse the data the old way (john's API)
 #get the results of one flight (by object id)
-df,dt,(x0,y0,obj_id,framenumber0,time0) = combine.get_one_result(4984)
+df,dt,(x0,y0,obj_id,framenumber0,time0) = combine.get_one_result(4233)
 
 #get all results (se the docs for the structure of the results dictionary)
 results, dt = combine.get_results()
