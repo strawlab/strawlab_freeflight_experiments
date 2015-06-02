67dea40f7ceb7bc126912218aef4a434a5ee5896
John Stowers <john.stowers@gmail.com>
2015-06-02 12:40:03 +0200

    sfe.conditions: tighten regexes to start/end string

--- src/strawlab_freeflight_experiments/conditions.py
+++ src/strawlab_freeflight_experiments/conditions.py
@@ -66,13 +66,13 @@ class ConditionCompat(OrderedDict):
     #fields in the experiment condition yaml file
 
     #https://regex101.com/r/tN5mQ4/1
-    ROTATION_RE = re.compile("(?P<cylinder_image>\w+\.png)/(?P<svg_path>\w+\.svg)/(?P<gain>[\d.+]+)/(?P<radius_when_locked>[\d.+-]+)/(?P<advance_threshold>[\d.+-]+)/(?P<z_gain>[\d.+]+)/?(?P<z_target>[\d.+-]+)?$")
+    ROTATION_RE = re.compile("^(?P<cylinder_image>\w+\.png)/(?P<svg_path>\w+\.svg)/(?P<gain>[\d.+]+)/(?P<radius_when_locked>[\d.+-]+)/(?P<advance_threshold>[\d.+-]+)/(?P<z_gain>[\d.+]+)/?(?P<z_target>[\d.+-]+)?$")
 
-    CONFLICT_RE = re.compile("(?P<cylinder_image>\w+\.png)/(?P<svg_path>\w+\.svg)(?:/[\d.+-]+){1,5}/(?P<model_descriptor>\w+\.osg(?:\|[\d.+-]+)+)$")
+    CONFLICT_RE = re.compile("^(?P<cylinder_image>\w+\.png)/(?P<svg_path>\w+\.svg)(?:/[\d.+-]+){1,5}/(?P<model_descriptor>\w+\.osg(?:\|[\d.+-]+)+)$")
 
-    PERTURB_RE = re.compile("\w+\.png\/\w+\.svg(?:\/[\d.+-]+){3,5}\/\w+\|.*$")
+    PERTURB_RE = re.compile("^\w+\.png\/\w+\.svg(?:\/[\d.+-]+){3,5}\/\w+\|.*$")
 
-    CONFINE_RE = re.compile("(?P<stimulus_filename>[\w.]+\.osg)/(?P<x0>[\d.+-]+)/(?P<y0>[\d.+-]+)/(?P<lag>[\d.+-]+)$")
+    CONFINE_RE = re.compile("^(?P<stimulus_filename>[\w.]+\.osg)/(?P<x0>[\d.+-]+)/(?P<y0>[\d.+-]+)/(?P<lag>[\d.+-]+)$")
 
     def __init__(self, slash_string):
         OrderedDict.__init__(self)
