535f7cd6bbf67c930cb111ba563460fd05231273
John Stowers <john.stowers@gmail.com>
2015-06-16 10:51:12 +0200

    confinement-statistics: plot more things

--- scripts/specialist/confinement-statistics.py
+++ scripts/specialist/confinement-statistics.py
@@ -2,6 +2,7 @@
 import sys
 import os.path
 import collections
+import math
 
 import numpy as np
 import pandas as pd
@@ -12,8 +13,14 @@ if not os.environ.get('DISPLAY'):
     matplotlib.use('agg')
 
 import matplotlib.patches
-
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+try:
+    import seaborn as sns
+    new_seaborn = int(sns.__version__.split('.')[1]) > 5
+except ImportError, e:
+    sns = None
 
 import roslib
 roslib.load_manifest('strawlab_freeflight_experiments')
@@ -60,8 +67,10 @@ def draw_lock_area(ax, df, prefix,**kwargs):
         flyflypath.mplview.plot_polygon(m,ax,**kwargs)
 
 def plot_confine_traces(combine, args, sorted_conditions):
-    figncols = combine.get_num_conditions()
-    figsize = (5.0*figncols,5.0)
+    figncols = 4
+    fignrows = max(1,int(math.ceil(combine.get_num_conditions() / 4.0)))
+    gs = gridspec.GridSpec(fignrows,figncols)
+    figsize = (5.0*figncols,5.0*fignrows)
     name = '%s_traces' % combine.fname
     arena = analysislib.arenas.get_arena_from_args(args)
     results,dt = combine.get_results()
@@ -69,9 +78,9 @@ def plot_confine_traces(combine, args, sorted_conditions):
         ax = None
         axes = set()
         for i,current_condition in enumerate(sorted_conditions):
-            r = results[current_condition]
 
-            ax = fig.add_subplot(1,figncols,1+i,sharex=ax,sharey=ax)
+            r = results[current_condition]
+            ax = fig.add_subplot(gs[i],sharex=ax,sharey=ax)
             axes.add( ax )
 
             if not r['count']:
@@ -96,11 +105,11 @@ def plot_confine_traces(combine, args, sorted_conditions):
         if aplt.WRAP_TEXT:
             fig.canvas.mpl_connect('draw_event', aplt.autowrap_text)
 
-def plot_confine_times(combine, args, sorted_conditions, skip_high=0, plot_filtered_trajectories=False):
+def plot_confine_times(combine, args, sorted_conditions, skip_high=np.inf, skip_short=-np.inf, plot_filtered_trajectories=False):
     results,dt = combine.get_results()
     arena = analysislib.arenas.get_arena_from_args(args)
 
-    data = {'condition':[],'time_in_area':[], 'percent_in_area':[]}
+    data = {'condition':[],'time_in_area':[], 'percent_in_area':[], 'dist_to_wall':[], 'radius':[], 'len':[]}
 
     for cond in sorted_conditions:
         r = results[cond]
@@ -126,8 +135,14 @@ def plot_confine_times(combine, args, sorted_conditions, skip_high=0, plot_filte
             in_area_time = in_area_len*dt
             in_area_pct = in_area_len / len(df)
 
-            if skip_high and (df['z'].head(10).mean() > skip_high):
+            dist_to_wall = [hm.distance_to_closest_point(x,y) if hm.contains(x,y) else 0 for x,y in zip(df['x'],df['y'])]
+
+            if df['z'].head(10).mean() > skip_high:
                 print "SKIP (too high)"
+                continue
+
+            if len(df) < skip_short:
+                print "SKIP (short)"
                 continue
 
             dfs.append(df)
@@ -135,6 +150,9 @@ def plot_confine_times(combine, args, sorted_conditions, skip_high=0, plot_filte
             data['condition'].append(condn)
             data['time_in_area'].append(in_area_time)
             data['percent_in_area'].append(in_area_pct)
+            data['dist_to_wall'].append(np.nanmean(dist_to_wall).astype(float))
+            data['radius'].append(df['radius'].mean())
+            data['len'].append(len(df))
 
         if plot_filtered_trajectories:
             f = plt.figure()
@@ -144,22 +162,19 @@ def plot_confine_times(combine, args, sorted_conditions, skip_high=0, plot_filte
             ax.set_title(condn)
             aplt.layout_trajectory_plots(ax, arena, False)
 
-    try:
-        import seaborn as sns
-        new_seaborn = int(sns.__version__.split('.')[1]) > 5
-    except ImportError, e:
+    if sns is None:
         print "SEABORN NOT AVAILABLE", e
         return
 
     df = pd.DataFrame(data)
 
-    for y,label in (('time_in_area','time in area (s)'), ('percent_in_area','time in area (pct)')):
+    for y,label in (('time_in_area','time in area (s)'), ('percent_in_area','time in area (pct)'), ('dist_to_wall', 'dist to wall (px)'), ('radius','distance from 0,0 (m)'), ('len','len (n)')):
         name = '%s_%s' % (combine.fname,y)
         with aplt.mpl_fig(name,args) as fig:
             ax = fig.add_subplot(1,1,1)
 
             if new_seaborn:
-                sns.boxplot(x='condition',y=y,data=df, ax=ax)
+                sns.boxplot(x='condition',y=y,data=df, ax=ax, fliersize=0.0)
                 sns.stripplot(x='condition',y=y,data=df, size=3, jitter=True, color="white", edgecolor="gray", ax=ax)
             else:
                 raise Exception("Not Supported")
@@ -167,6 +182,10 @@ def plot_confine_times(combine, args, sorted_conditions, skip_high=0, plot_filte
             for label in ax.get_xticklabels():
                 label.set_ha('right')
                 label.set_rotation(30)
+
+            if y == 'time_in_area':
+                ax.set_ylim(0,20.0)
+
 
         name = '%s_%shist' % (combine.fname,y)
         with aplt.mpl_fig(name,args) as fig:
@@ -181,7 +200,11 @@ def plot_confine_times(combine, args, sorted_conditions, skip_high=0, plot_filte
             ax.legend()
 
             if y == 'percent_in_area':
-                ax.set_xlim(0,1.0)
+                ax.set_xlim(0,1.2)
+            if y == 'radius':
+                ax.set_xlim(0,0.5)
+
+            ax.set_xlabel(y)
 
 
 if __name__=='__main__':
@@ -215,7 +238,7 @@ if __name__=='__main__':
         sorted_conditions.append(zero_texture)
 
     plot_confine_traces(combine, args, sorted_conditions)
-    plot_confine_times(combine, args, sorted_conditions,skip_high=0.85)
+    plot_confine_times(combine, args, sorted_conditions,skip_high=np.inf)
 
     if args.show:
         aplt.show_plots()
