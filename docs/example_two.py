4759921e9b71d88724838eeb5bfb7ab19d39ea58
John Stowers <john.stowers@gmail.com>
2015-07-16 21:37:59 +0200

    docs: save fig

--- docs/example_two.py
+++ docs/example_two.py
@@ -14,7 +14,7 @@ combine.add_from_uuid(UUID)
 #get the results of one flight (by object id)
 df,dt,(x0,y0,obj_id,framenumber0,time0,condition,uuid) = combine.get_one_result(4233, framenumber0=737996)
 
-print dt
+assert dt == 1/100.0
 
 ax = plt.figure('xy').add_subplot(1,1,1)
 ax.plot(df['x'].values,df['y'].values)
@@ -24,9 +24,12 @@ vy = np.gradient(df['y']) / dt
 theta = np.unwrap(np.arctan2(vy,vx))
 dtheta = np.gradient(theta) / dt
 
-ax2 = plt.figure('dthetas', figsize=(18,6)).add_subplot(1,1,1)
+f = plt.figure('dthetas', figsize=(18,6))
+ax2 = f.add_subplot(1,1,1)
 ax2.plot(dtheta,'b-',label='manual')
 ax2.plot(df['dtheta'].values,'r-',label='combine')
 ax2.legend()
+
+f.savefig('dtheta.png')
 
 plt.show()
