2f77b8c1e83f9a12612c4df18c3ced28597fc1a1
Andrew Straw <strawman@astraw.com>
2016-04-15 14:17:12 +0200

    move analysislib to strawlab-analysis-kitchen-sink

--- src/analysislib/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import progressbar
-
-def get_progress_bar(name, maxval):
-    widgets = ["%s: " % name, progressbar.Percentage(),
-               progressbar.Bar(), progressbar.ETA()]
-    pbar = progressbar.ProgressBar(widgets=widgets,maxval=maxval).start()
-    return pbar
-
