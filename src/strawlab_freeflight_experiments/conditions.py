e261cb8d8d75da80d9070f615e750d362d1202f7
Andrew Straw <strawman@astraw.com>
2016-05-23 11:31:59 +0200

    fix move to kitchensink repo

--- src/strawlab_freeflight_experiments/conditions.py
+++ src/strawlab_freeflight_experiments/conditions.py
@@ -7,3 +7,5 @@ if int(os.environ.get('STRAWLAB_THROW_DEPRECATION', '0')):
 warnings.warn("You are importing module "
               "'strawlab_freeflight_experiments.conditions'. Please update this "
               "to module 'freeflight_analysis.conditions'")
+
+from freeflight_analysis.conditions import *
